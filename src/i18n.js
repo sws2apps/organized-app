d3f353c91744438bdb3f30c547a00c1c69921909
rhahao <rhahao.vj@gmail.com>
2021-12-27 21:50:23 +0300

    fix(i18n): change path to import english translation file

--- src/i18n.js
+++ src/i18n.js
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
-import translationEN from './locales/en.json';
+import translationEN from './locales/e.json';
 import translationMG from './locales/mg.json';
 
 const resources = {
