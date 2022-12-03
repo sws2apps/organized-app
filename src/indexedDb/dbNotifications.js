615728317a56e29b14ab838a0f6a23dd8927ff10
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2022-12-03 14:36:23 +0300

    fix(notifications): update for new api response

--- src/indexedDb/dbNotifications.js
+++ src/indexedDb/dbNotifications.js
@@ -4,57 +4,53 @@ import { langList } from '../locales/langList';
 import appDb from './mainDb';
 
 export const dbGetNotifications = async () => {
-	const notifications = await appDb.table('notifications').toArray();
+  const notifications = await appDb.table('notifications').toArray();
 
-	return notifications;
+  return notifications;
 };
 
 export const dbSaveNotifications = async (data) => {
-	let notifications = await dbGetNotifications();
+  let notifications = await dbGetNotifications();
 
-	await appDb.notifications.clear();
+  await appDb.notifications.clear();
 
-	for (let i = 0; i < data.length; i++) {
-		const announcement = data[i];
+  for (let i = 0; i < data.length; i++) {
+    const announcement = data[i];
 
-		let obj = {};
-		const notification = notifications.find(
-			(item) => item.notification_id === announcement.id
-		);
+    let obj = {};
+    const notification = notifications.find((item) => item.notification_id === announcement.id);
 
-		if (notification) {
-			obj.isRead = notification.isRead;
-		} else {
-			obj.isRead = false;
-		}
-		obj.notification_id = announcement.id;
-		obj.content = {};
-		langList.forEach((lang) => {
-			const code = lang.code.toUpperCase();
-			obj.content[code] = announcement[code];
-		});
+    if (notification) {
+      obj.isRead = notification.isRead;
+    } else {
+      obj.isRead = false;
+    }
+    obj.notification_id = announcement.id;
+    obj.content = {};
+    langList.forEach((lang) => {
+      const code = lang.code.toUpperCase();
+      obj.content[code] = announcement.data[code];
+    });
 
-		await appDb.notifications.add(obj);
-	}
+    await appDb.notifications.add(obj);
+  }
 
-	notifications = await dbGetNotifications();
+  notifications = await dbGetNotifications();
 
-	await promiseSetRecoil(appNotificationsState, notifications);
-	return;
+  await promiseSetRecoil(appNotificationsState, notifications);
+  return;
 };
 
 export const dbReadNotification = async (id) => {
-	let notifications = await dbGetNotifications();
+  let notifications = await dbGetNotifications();
 
-	const notification = notifications.find(
-		(item) => item.notification_id === id
-	);
+  const notification = notifications.find((item) => item.notification_id === id);
 
-	if (notification) {
-		await appDb.notifications.update(notification.id, { isRead: true });
-	}
+  if (notification) {
+    await appDb.notifications.update(notification.id, { isRead: true });
+  }
 
-	notifications = await dbGetNotifications();
-	await promiseSetRecoil(appNotificationsState, notifications);
-	return;
+  notifications = await dbGetNotifications();
+  await promiseSetRecoil(appNotificationsState, notifications);
+  return;
 };
