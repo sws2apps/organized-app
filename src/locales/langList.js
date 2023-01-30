6e3b1db33e16ba9f987241c3effad690d52a3341
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2023-01-30 23:14:45 +0300

    chore(app): update source folder name for crowdin

--- src/locales/langList.js
+++ src/locales/langList.js
@@ -1,4 +1,4 @@
 export const LANGUAGE_LIST = [
-  { code: 'e', locale: 'e', name: 'English', isUI: true, isSource: true },
-  { code: 'mg', locale: 'mg-MG', name: 'Malagasy', isUI: true, isSource: true },
+	{ code: 'e', locale: 'en', name: 'English', isUI: true, isSource: true },
+	{ code: 'mg', locale: 'mg-MG', name: 'Malagasy', isUI: true, isSource: true },
 ];
