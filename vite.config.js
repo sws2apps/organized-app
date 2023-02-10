d889b2aa26606fdedd49ca3dcea98002c1ba8e6e
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2023-02-10 15:24:55 +0300

    fix(csp): update base-uri value

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
 import { loadVersion } from '@sws2apps/vite-plugin-package-version';
-import { comlink } from 'vite-plugin-comlink';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), comlink(), eslint(), loadVersion()],
-  worker: { plugins: [comlink()] },
+  plugins: [react(), eslint(), loadVersion()],
   server: {
     port: 4000,
     host: true,
