ece68d9754e64323957c1135fc1bdc604ca88468
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2023-02-11 23:12:22 +0300

    chore(localize): localize user account upgrade component

--- src/features/startup/UserAccountUpgrade.jsx
+++ src/features/startup/UserAccountUpgrade.jsx
@@ -17,139 +17,132 @@ import OAuth from './OAuth';
 import EmailAuth from './EmailAuth';
 
 const UserAccountUpgrade = () => {
-  const cancel = useRef();
+	const cancel = useRef();
 
-  const { t } = useTranslation('ui');
+	const { t } = useTranslation('ui');
 
-  const setAppSnackOpen = useSetRecoilState(appSnackOpenState);
-  const setAppSeverity = useSetRecoilState(appSeverityState);
-  const setAppMessage = useSetRecoilState(appMessageState);
-  const setIsOAuthAccountUpgrade = useSetRecoilState(isOAuthAccountUpgradeState);
+	const setAppSnackOpen = useSetRecoilState(appSnackOpenState);
+	const setAppSeverity = useSetRecoilState(appSeverityState);
+	const setAppMessage = useSetRecoilState(appMessageState);
+	const setIsOAuthAccountUpgrade = useSetRecoilState(isOAuthAccountUpgradeState);
 
-  const visitorID = useRecoilValue(visitorIDState);
-  const isOnline = useRecoilValue(isOnlineState);
-  const isEmailAuth = useRecoilValue(isEmailAuthState);
+	const visitorID = useRecoilValue(visitorIDState);
+	const isOnline = useRecoilValue(isOnlineState);
+	const isEmailAuth = useRecoilValue(isEmailAuthState);
 
-  const [userTmpEmail, setUserTmpEmail] = useState('');
-  const [userTmpPwd, setUserTmpPwd] = useState('');
-  const [showPwd, setShowPwd] = useState(false);
-  const [isLogin, setIsLogin] = useState(true);
-  const [isProcessing, setIsProcessing] = useState(false);
-  const [hasErrorEmail, setHasErrorEmail] = useState(false);
-  const [hasErrorPwd, setHasErrorPwd] = useState(false);
+	const [userTmpEmail, setUserTmpEmail] = useState('');
+	const [userTmpPwd, setUserTmpPwd] = useState('');
+	const [showPwd, setShowPwd] = useState(false);
+	const [isLogin, setIsLogin] = useState(true);
+	const [isProcessing, setIsProcessing] = useState(false);
+	const [hasErrorEmail, setHasErrorEmail] = useState(false);
+	const [hasErrorPwd, setHasErrorPwd] = useState(false);
 
-  const handleEmailChange = (value) => {
-    value = value.toLowerCase();
-    setUserTmpEmail(value);
-  };
+	const handleEmailChange = (value) => {
+		value = value.toLowerCase();
+		setUserTmpEmail(value);
+	};
 
-  const togglePwd = () => {
-    setShowPwd((prev) => {
-      return !prev;
-    });
-  };
+	const togglePwd = () => {
+		setShowPwd((prev) => {
+			return !prev;
+		});
+	};
 
-  const handleAuthSignIn = async () => {
-    try {
-      cancel.current = false;
-      setHasErrorEmail(false);
-      setHasErrorPwd(false);
-      if (isEmailValid(userTmpEmail) && userTmpPwd.length >= 10) {
-        setIsProcessing(true);
-        const auth = getAuth();
-        await signInWithEmailAndPassword(auth, userTmpEmail, userTmpPwd);
-        setIsProcessing(false);
-        setIsOAuthAccountUpgrade(true);
-        setIsLogin(false);
-      } else {
-        if (!isEmailValid(userTmpEmail)) {
-          setHasErrorEmail(true);
-        }
-        if (userTmpPwd.length < 10) {
-          setHasErrorPwd(true);
-        }
-      }
-    } catch (err) {
-      if (!cancel.current) {
-        setIsProcessing(false);
-        setAppMessage(t('incorrectInfo'));
-        setAppSeverity('error');
-        setAppSnackOpen(true);
-      }
-    }
-  };
+	const handleAuthSignIn = async () => {
+		try {
+			cancel.current = false;
+			setHasErrorEmail(false);
+			setHasErrorPwd(false);
+			if (isEmailValid(userTmpEmail) && userTmpPwd.length >= 10) {
+				setIsProcessing(true);
+				const auth = getAuth();
+				await signInWithEmailAndPassword(auth, userTmpEmail, userTmpPwd);
+				setIsProcessing(false);
+				setIsOAuthAccountUpgrade(true);
+				setIsLogin(false);
+			} else {
+				if (!isEmailValid(userTmpEmail)) {
+					setHasErrorEmail(true);
+				}
+				if (userTmpPwd.length < 10) {
+					setHasErrorPwd(true);
+				}
+			}
+		} catch (err) {
+			if (!cancel.current) {
+				setIsProcessing(false);
+				setAppMessage(t('incorrectInfo'));
+				setAppSeverity('error');
+				setAppSnackOpen(true);
+			}
+		}
+	};
 
-  useEffect(() => {
-    const auth = getAuth();
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setIsLogin(false);
-        setIsOAuthAccountUpgrade(true);
-      } else {
-        setIsLogin(true);
-      }
-    });
-  }, [setIsOAuthAccountUpgrade]);
+	useEffect(() => {
+		const auth = getAuth();
+		onAuthStateChanged(auth, (user) => {
+			if (user) {
+				setIsLogin(false);
+				setIsOAuthAccountUpgrade(true);
+			} else {
+				setIsLogin(true);
+			}
+		});
+	}, [setIsOAuthAccountUpgrade]);
 
-  return (
-    <Box sx={{ maxWidth: '500px', margin: '20px auto' }}>
-      <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px', marginBottom: '20px' }}>
-        <GppMaybeIcon color="warning" sx={{ fontSize: '40px' }} />
-        <Typography sx={{ fontSize: '20px' }}>User Account Upgrade</Typography>
-      </Box>
+	return (
+		<Box sx={{ maxWidth: '500px', margin: '20px auto' }}>
+			<Box sx={{ display: 'flex', alignItems: 'center', gap: '8px', marginBottom: '20px' }}>
+				<GppMaybeIcon color='warning' sx={{ fontSize: '40px' }} />
+				<Typography sx={{ fontSize: '20px' }}>{t('userAccountUpgrade')}</Typography>
+			</Box>
 
-      {isLogin && (
-        <Box>
-          <Box sx={{ display: 'flex', flexDirection: 'column', gap: '15px' }}>
-            <Typography>
-              CPE has been updated to use a new authentication method. Email and password login is no longer supported.
-              Instead, you will use a service provider, like Google, Yahoo, Microsoft, to login into your account. If
-              you do not have account in any of these providers, you can use the passwordless authentication. Please
-              note that entering verification code from an authenticator app is always required.
-            </Typography>
-            <Typography>To begin, login into your account using your usual email address and password.</Typography>
-          </Box>
+			{isLogin && (
+				<Box>
+					<Box sx={{ display: 'flex', flexDirection: 'column', gap: '15px' }}>
+						<Typography>{t('userAccountUpgradeDesc')}</Typography>
+						<Typography>{t('userAccountUpgradeStart')}</Typography>
+					</Box>
 
-          <Box sx={{ maxWidth: '380px' }}>
-            <UserEmailField
-              userEmail={userTmpEmail}
-              setUserEmail={(value) => handleEmailChange(value)}
-              hasErrorEmail={hasErrorEmail}
-            />
-            <UserPasswordField
-              userPwd={userTmpPwd}
-              setUserPwd={(value) => setUserTmpPwd(value)}
-              showPwd={showPwd}
-              label={t('password')}
-              hasErrorPwd={hasErrorPwd}
-            />
-            <TogglePassword showPwd={showPwd} togglePwd={togglePwd} />
-            <Box sx={{ marginTop: '10px', display: 'flex', justifyContent: 'flex-end' }}>
-              <Button
-                variant="contained"
-                disabled={!isOnline || isProcessing || visitorID.length === 0}
-                endIcon={isProcessing ? <CircularProgress size={25} /> : null}
-                onClick={handleAuthSignIn}
-              >
-                {t('signIn')}
-              </Button>
-            </Box>
-          </Box>
-        </Box>
-      )}
+					<Box sx={{ maxWidth: '380px' }}>
+						<UserEmailField
+							userEmail={userTmpEmail}
+							setUserEmail={(value) => handleEmailChange(value)}
+							hasErrorEmail={hasErrorEmail}
+						/>
+						<UserPasswordField
+							userPwd={userTmpPwd}
+							setUserPwd={(value) => setUserTmpPwd(value)}
+							showPwd={showPwd}
+							label={t('password')}
+							hasErrorPwd={hasErrorPwd}
+						/>
+						<TogglePassword showPwd={showPwd} togglePwd={togglePwd} />
+						<Box sx={{ marginTop: '10px', display: 'flex', justifyContent: 'flex-end' }}>
+							<Button
+								variant='contained'
+								disabled={!isOnline || isProcessing || visitorID.length === 0}
+								endIcon={isProcessing ? <CircularProgress size={25} /> : null}
+								onClick={handleAuthSignIn}
+							>
+								{t('signIn')}
+							</Button>
+						</Box>
+					</Box>
+				</Box>
+			)}
 
-      {!isLogin && !isEmailAuth && (
-        <Box>
-          <Typography sx={{ marginBottom: '15px' }}>
-            Choose below which service you would like to use to upgrade your account.
-          </Typography>
-          <OAuth />
-        </Box>
-      )}
+			{!isLogin && !isEmailAuth && (
+				<Box>
+					<Typography sx={{ marginBottom: '15px' }}>{t('userAccountUpgradeChooseService')}</Typography>
+					<OAuth />
+				</Box>
+			)}
 
-      {!isLogin && isEmailAuth && <EmailAuth />}
-    </Box>
-  );
+			{!isLogin && isEmailAuth && <EmailAuth />}
+		</Box>
+	);
 };
 
 export default UserAccountUpgrade;
