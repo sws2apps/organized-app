03c2cbc69cad2d66832532bae3e70f85f87cba19
rhahao <26148770+rhahao@users.noreply.github.com>
2023-02-20 06:50:21 +0300

    feat(app): add firebase analytics

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,10 +1,21 @@
 import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID,
+};
+
+const firebaseAnalyticsConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_ANALYTICS_APIKEY,
+  authDomain: import.meta.env.VITE_FIREBASE_ANALYTICS_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_ANALYTICS_PROJECTID,
+  appId: import.meta.env.VITE_FIREBASE_ANALYTICS_APPID,
+  measurementId: import.meta.env.VITE_FIREBASE_ANALYTICS_MEASUREMENTID,
 };
 
 initializeApp(firebaseConfig);
+
+const appAnalytics = initializeApp(firebaseAnalyticsConfig, 'appAnalytics');
+getAnalytics(appAnalytics);
