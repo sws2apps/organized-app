966e5d465df10a8e5790329ccda44b3d685d003d
rhahao <26148770+rhahao@users.noreply.github.com>
2023-03-09 22:25:03 +0300

    fix(settings): fix invalid time error when opening settings page

--- src/components/ErrorBoundary.jsx
+++ src/components/ErrorBoundary.jsx
@@ -1,22 +1,26 @@
 import { useRouteError } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import NavBar from './NavBar';
 
 const ErrorBoundary = () => {
   const error = useRouteError();
 
   return (
-    <Box
-      sx={{
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%,-50%)',
-      }}
-    >
-      <Typography>Ooops</Typography>
-      <Typography>{error.message || error.data}</Typography>
-    </Box>
+    <>
+      <NavBar />
+      <Box
+        sx={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%,-50%)',
+        }}
+      >
+        <Typography>Ooops</Typography>
+        <Typography>{error.message || error.data}</Typography>
+      </Box>
+    </>
   );
 };
 
