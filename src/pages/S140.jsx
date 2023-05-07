b5ba6bf48d0166e5e823c75a6f516edde8b054fd
rhahao <26148770+rhahao@users.noreply.github.com>
2023-05-07 20:45:31 +0300

    fix(schedules): omit preview for S-140 template

--- src/pages/S140.jsx
+++ src/pages/S140.jsx
@@ -1,8 +1,8 @@
-import { lazy, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import Box from '@mui/material/Box';
-import S140Previewer from '../views/S140MainView'
+import S140Previewer from '../views/S140MainView';
 import { rootModalOpenState } from '../states/main';
 import { currentScheduleState } from '../states/schedule';
 import { Schedules } from '../classes/Schedules';
