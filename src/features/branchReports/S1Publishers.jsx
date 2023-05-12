4a2b060f78a67361e831392a362c2b2efe28975a
rhahao <26148770+rhahao@users.noreply.github.com>
2023-05-12 20:36:34 +0300

    feat(reports): change layout of S1 report

--- src/features/branchReports/S1Publishers.jsx
+++ src/features/branchReports/S1Publishers.jsx
@@ -1,4 +1,5 @@
 import { useTranslation } from 'react-i18next';
+import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import S1Header from './S1Header';
 import S1Field from './S1Field';
@@ -10,19 +11,22 @@ const S1Publishers = ({
   publishersHours,
   publishersReturnVisits,
   publishersBibleStudies,
+  gridViews,
 }) => {
   const { t } = useTranslation('ui');
 
   return (
-    <Paper sx={{ display: 'flex', flexDirection: 'column', gap: '15px', padding: '20px' }}>
-      <S1Header header={t('publishers')} />
-      <S1Field field="numberReports" value={publishersReports} />
-      <S1Field field="placements" value={publishersPlacements} />
-      <S1Field field="videos" value={publishersVideos} />
-      <S1Field field="hours" value={publishersHours} />
-      <S1Field field="returnVisits" value={publishersReturnVisits} />
-      <S1Field field="bibleStudies" value={publishersBibleStudies} />
-    </Paper>
+    <Grid item xs={gridViews.xs} sm={gridViews.sm} md={gridViews.md} lg={gridViews.lg}>
+      <Paper sx={{ display: 'flex', flexDirection: 'column', gap: '15px', padding: '20px' }}>
+        <S1Header header={t('publishers')} />
+        <S1Field field="numberReports" value={publishersReports} />
+        <S1Field field="placements" value={publishersPlacements} />
+        <S1Field field="videos" value={publishersVideos} />
+        <S1Field field="hours" value={publishersHours} />
+        <S1Field field="returnVisits" value={publishersReturnVisits} />
+        <S1Field field="bibleStudies" value={publishersBibleStudies} />
+      </Paper>
+    </Grid>
   );
 };
 
