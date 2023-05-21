a01fa1e5345a578afa2bce4d607f97c99003fae6
rhahao <26148770+rhahao@users.noreply.github.com>
2023-05-21 10:51:51 +0300

    feat(app): add option to reset CPE data on error

--- src/components/ErrorBoundary.jsx
+++ src/components/ErrorBoundary.jsx
@@ -1,10 +1,25 @@
 import { useRouteError } from 'react-router-dom';
+import { getAuth, signOut } from 'firebase/auth';
+import { useTranslation } from 'react-i18next';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import NavBar from './NavBar';
+import { deleteAppDb } from '../indexedDb/dbUtility';
 
 const ErrorBoundary = () => {
   const error = useRouteError();
+
+  const { t } = useTranslation('ui');
+
+  const handleDelete = async () => {
+    const auth = await getAuth();
+
+    await deleteAppDb();
+    await signOut(auth);
+
+    window.location.href = './';
+  };
 
   return (
     <>
@@ -19,6 +34,12 @@ const ErrorBoundary = () => {
       >
         <Typography>Ooops</Typography>
         <Typography>{error.message || error.data}</Typography>
+        <Typography sx={{ marginTop: '15px' }} color="error">
+          {t('errorResetData')}
+        </Typography>
+        <Button variant="contained" color="error" sx={{ marginTop: '10px' }} onClick={handleDelete}>
+          {t('reset')}
+        </Button>
       </Box>
     </>
   );
