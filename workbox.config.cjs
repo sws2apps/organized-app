d362944a35661d60dc7abdfbc6df0ba90fab9bf5
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2023-08-14 20:06:10 +0300

    fix(workbox): adjust extension for font file

--- workbox.config.cjs
+++ workbox.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
 	globDirectory: 'dist/',
-	globPatterns: ['**/*.{ico,json,png,html,js,css,webmanifest,pdf,svg,woff,woff2,tff}'],
+	globPatterns: ['**/*.{ico,json,png,html,js,css,webmanifest,pdf,svg,woff,woff2,ttf}'],
 	ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
 	swDest: 'dist/service-worker.js',
 	maximumFileSizeToCacheInBytes: 10000000,
