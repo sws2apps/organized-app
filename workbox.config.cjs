015c66eeb2ac02a83cab65e5543847395adb3f62
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2023-08-14 19:57:28 +0300

    fix(workbox): add tff file to be cached offline by service worker

--- workbox.config.cjs
+++ workbox.config.cjs
@@ -1,8 +1,8 @@
 module.exports = {
-  globDirectory: 'dist/',
-  globPatterns: ['**/*.{ico,json,png,html,js,css,webmanifest,pdf,svg,woff,woff2}'],
-  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
-  swDest: 'dist/service-worker.js',
-  maximumFileSizeToCacheInBytes: 10000000,
-  sourcemap: false,
+	globDirectory: 'dist/',
+	globPatterns: ['**/*.{ico,json,png,html,js,css,webmanifest,pdf,svg,woff,woff2,tff}'],
+	ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
+	swDest: 'dist/service-worker.js',
+	maximumFileSizeToCacheInBytes: 10000000,
+	sourcemap: false,
 };
