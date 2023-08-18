1954dfa112b85f17545773b7c039eb83cc7570ff
rhahao <26148770+rhahao@users.noreply.github.com>
2023-08-18 17:52:20 +0300

    feat(locales): support German language

--- src/locales/langList.js
+++ src/locales/langList.js
@@ -1,9 +1,10 @@
 export const LANGUAGE_LIST = [
-	{ code: 'e', locale: 'en', name: 'English', isUI: true, isSource: true, hasEPUB: true },
-	{ code: 'mg', locale: 'mg-MG', name: 'Malagasy', isUI: true, isSource: true, hasEPUB: true },
-	{ code: 't', locale: 'pt-BR', name: 'Português (Brasil)', isUI: true, isSource: true, hasEPUB: false },
-	{ code: 'tnd', locale: 'mg-TND', name: 'Tandroy', isUI: false, isSource: true, hasEPUB: true },
-	{ code: 'tnk', locale: 'mg-TNK', name: 'Tankarana', isUI: false, isSource: true, hasEPUB: true },
-	{ code: 'vz', locale: 'mg-VZ', name: 'Vezo', isUI: false, isSource: true, hasEPUB: true },
-	{ code: 'k', locale: 'uk-UA', name: 'Українська', isUI: true, isSource: true, hasEPUB: true },
+  { code: 'e', locale: 'en', name: 'English', isUI: true, isSource: true, hasEPUB: true },
+  { code: 'x', locale: 'de-DE', name: 'German', isUI: true, isSource: true, hasEPUB: true },
+  { code: 'mg', locale: 'mg-MG', name: 'Malagasy', isUI: true, isSource: true, hasEPUB: true },
+  { code: 't', locale: 'pt-BR', name: 'Português (Brasil)', isUI: true, isSource: true, hasEPUB: false },
+  { code: 'tnd', locale: 'mg-TND', name: 'Tandroy', isUI: false, isSource: true, hasEPUB: true },
+  { code: 'tnk', locale: 'mg-TNK', name: 'Tankarana', isUI: false, isSource: true, hasEPUB: true },
+  { code: 'vz', locale: 'mg-VZ', name: 'Vezo', isUI: false, isSource: true, hasEPUB: true },
+  { code: 'k', locale: 'uk-UA', name: 'Українська', isUI: true, isSource: true, hasEPUB: true },
 ];
