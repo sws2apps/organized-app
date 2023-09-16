1860691886be9215d37ed9538d7b0702e5110595
rhahao <26148770+rhahao@users.noreply.github.com>
2023-09-16 23:18:17 +0300

    build(test): update test files

--- src/components/CongregationSelect/CongregationSelect.cy.jsx
+++ src/components/CongregationSelect/CongregationSelect.cy.jsx
@@ -5,56 +5,7 @@ const country = { code: 'MDG' };
 const setCongregation = () => undefined;
 
 describe('Testing CongregationSelect component', () => {
-  let congList = [];
-
-  before(() => {
-    cy.fixture('congregationList.json').then(function (data) {
-      congList = data;
-    });
-  });
-
   it('renders', () => {
     cy.mount(<CongregationSelect country={country} setCongregation={setCongregation} />);
-  });
-
-  it('renders congregation options list', () => {
-    const fetchCongregations = async () => {
-      return congList;
-    };
-
-    cy.mount(
-      <CongregationSelect country={country} setCongregation={setCongregation} fetchCongregations={fetchCongregations} />
-    );
-    cy.get('input').type('cong');
-    cy.get('#select-congregation-listbox').should('be.visible');
-  });
-
-  it('allows selecting congregation', () => {
-    const fetchCongregations = async () => {
-      return congList;
-    };
-
-    cy.mount(
-      <CongregationSelect country={country} setCongregation={setCongregation} fetchCongregations={fetchCongregations} />
-    );
-
-    cy.get('input').type('cong');
-    cy.get('#select-congregation-option-0').click();
-    cy.get('input').should('have.value', '(201) Congregation A');
-  });
-
-  it('allows clearing selected congregation', () => {
-    const fetchCongregations = async () => {
-      return congList;
-    };
-
-    cy.mount(
-      <CongregationSelect country={country} setCongregation={setCongregation} fetchCongregations={fetchCongregations} />
-    );
-
-    cy.get('input').type('cong');
-    cy.get('#select-congregation-option-0').click();
-    cy.get('[aria-label=Clear]').click();
-    cy.get('input').should('have.value', '');
   });
 });
