eda06a3e6d2fa69a627abaa81cd9a1b67b92cfc3
rhahao <26148770+rhahao@users.noreply.github.com>
2023-11-09 20:11:36 +0300

    test(cypress): update component setup

--- cypress/support/component.jsx
+++ cypress/support/component.jsx
@@ -2,7 +2,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands';
-import '../../src/i18n';
+import '../../src/current/i18n';
 
 import { mount } from 'cypress/react18';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
