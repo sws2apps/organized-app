dfb022120dae2dcaebdb39dd88f4f9aa5d03af39
rhahao <26148770+rhahao@users.noreply.github.com>
2023-11-11 00:20:24 +0300

    eslint setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  globals: {
+    __dirname: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'src/current'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: {
+    react: { version: '18.2' },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx'],
+      },
+      alias: {
+        extensions: ['.js', '.jsx'],
+        map: [
+          ['@assets', './src/v3/assets'],
+          ['@components', './src/v3/components'],
+        ],
+      },
+    },
+  },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+  },
+};
