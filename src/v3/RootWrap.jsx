983f5acedad6fd9c07caf078dae09bcf486f83ae
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2023-11-14 10:31:41 +0300

    use custom inter fonts

--- src/v3/RootWrap.jsx
+++ src/v3/RootWrap.jsx
@@ -1,38 +1,33 @@
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import { CacheProvider } from '@emotion/react';
 import createCache from '@emotion/cache';
-import '@fontsource/inter/200.css';
-import '@fontsource/inter/300.css';
-import '@fontsource/inter/400.css';
-import '@fontsource/inter/500.css';
-import '@fontsource/inter/600.css';
-import '@fontsource/inter/700.css';
 import App from './App';
+import './inter.css';
 import './global.css';
 import './index.css';
 
 const cache = createCache({
-  key: 'css',
-  prepend: true,
+	key: 'css',
+	prepend: true,
 });
 
 const theme = createTheme({
-  typography: {
-    allVariants: {
-      fontFamily: 'Inter',
-    },
-  },
+	typography: {
+		allVariants: {
+			fontFamily: 'Inter',
+		},
+	},
 });
 
 const RootWrap = () => {
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <CacheProvider value={cache}>
-        <App />
-      </CacheProvider>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<CssBaseline />
+			<CacheProvider value={cache}>
+				<App />
+			</CacheProvider>
+		</ThemeProvider>
+	);
 };
 
 export default RootWrap;
