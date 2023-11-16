9a8ae43aa6b2cf57bdc58bf2e739e12b04d3e960
rhahao <26148770+rhahao@users.noreply.github.com>
2023-11-16 07:11:05 +0300

    refactor(app): move locales folder to shared

--- src/current/i18n.js
+++ src/current/i18n.js
@@ -1,6 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import { LANGUAGE_LIST } from './locales/langList';
+import { LANGUAGE_LIST } from '../shared/locales/langList';
 
 const resources = {};
 
