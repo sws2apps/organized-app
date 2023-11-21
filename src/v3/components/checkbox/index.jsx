5b714f3980c96cd5bdeb8fd3fceb92a43a9eb300
rhahao <26148770+rhahao@users.noreply.github.com>
2023-11-21 06:55:44 +0300

    remove ref in checkbox component

--- src/v3/components/checkbox/index.jsx
+++ src/v3/components/checkbox/index.jsx
@@ -1,18 +1,14 @@
 import PropTypes from 'prop-types';
 import { Checkbox, FormControlLabel } from '@mui/material';
-import { IconCheckboxEmpty, IconCheckboxFilled, IconCheckboxMultiple } from '@icons';
 import { Typography } from '@components';
-import { useRef } from 'react';
+import { IconCheckboxEmpty, IconCheckboxFilled, IconCheckboxMultiple } from '@icons';
 
 const CPECheckbox = ({ checked = false, indeterminate = false, disabled = false, onChange, label = '' }) => {
-  const checkBoxRef = useRef();
-
   return (
     <FormControlLabel
       sx={{ marginLeft: '-4px', display: 'flex', alignItems: 'center', gap: '8px' }}
       control={
         <Checkbox
-          ref={checkBoxRef}
           checked={checked}
           indeterminate={indeterminate}
           disabled={disabled}
