4374fbd3fd84a7145bd9428d88db79b613ad6ac7
rhahao <26148770+rhahao@users.noreply.github.com>
2023-11-25 17:40:31 +0300

    update logic action location

--- src/v3/features/app_start/vip/oauth/components/button_base/useButtonBase.jsx
+++ src/v3/features/app_start/vip/oauth/components/button_base/useButtonBase.jsx
@@ -1,10 +1,30 @@
 import { useRecoilValue } from 'recoil';
 import { currentProviderState, isAuthProcessingState, isUserSignInState, isUserSignUpState } from '@states/app';
 import { setAuthPersistence, userSignInPopup } from '@services/firebase/auth';
-import { displayOnboardingFeedback, setIsEmailAuth, setIsUserSignIn, setIsUserSignUp } from '@services/recoil/app';
+import {
+  displayOnboardingFeedback,
+  setCongAccountConnected,
+  setCurrentMFAStage,
+  setIsAppLoad,
+  setIsAuthProcessing,
+  setIsCongAccountCreate,
+  setIsEmailAuth,
+  setIsSetup,
+  setIsUnauthorizedRole,
+  setIsUserSignIn,
+  setIsUserSignUp,
+  setRootModalOpen,
+  setUserMfaVerify,
+} from '@services/recoil/app';
 import useAppTranslation from '@hooks/useAppTranslation';
 import { useFeedback } from '@features/app_start';
 import { getMessageByCode } from '@services/i18n/translation';
+import { apiSendAuthorization } from '@services/api/user';
+import { CPE_ROLES } from '@constants/index';
+import { runUpdater, updateUserInfoAfterLogin } from '@services/cpe';
+import { handleUpdateSetting } from '@services/dexie/settings';
+import { apiFetchSchedule } from '@services/api/schedule';
+import { handleUpdateScheduleFromRemote } from '@services/cpe/schedules';
 
 const useButtonBase = ({ provider, isEmail }) => {
   const { t } = useAppTranslation();
@@ -17,13 +37,105 @@ const useButtonBase = ({ provider, isEmail }) => {
   const { showMessage, hideMessage } = useFeedback();
 
   const handleOAuthAction = async () => {
-    if (isAuthProcessing) return;
-
-    hideMessage();
-
     try {
+      if (isAuthProcessing) return;
+
+      hideMessage();
+
       await setAuthPersistence();
-      await userSignInPopup(provider);
+      const result = await userSignInPopup(provider);
+
+      if (result) {
+        setIsAuthProcessing(true);
+
+        const { status, data } = await apiSendAuthorization();
+
+        if (status !== 200) {
+          await displayOnboardingFeedback({
+            title: t('errorTitle'),
+            message: getMessageByCode(data.message),
+          });
+
+          showMessage();
+
+          setIsAuthProcessing(false);
+          return;
+        }
+
+        const result = {};
+        const { cong_name, cong_role, mfa } = data;
+
+        if (mfa === 'not_enabled') {
+          if (cong_name.length === 0) {
+            result.createCongregation = true;
+          }
+
+          if (cong_name.length > 0 && cong_role.length === 0) {
+            result.unauthorized = true;
+          }
+
+          if (cong_name.length > 0 && cong_role.length > 0) {
+            const approvedRole = cong_role.some((role) => CPE_ROLES.includes(role));
+
+            if (!approvedRole) {
+              result.unauthorized = true;
+            }
+
+            if (approvedRole) {
+              await updateUserInfoAfterLogin(data);
+
+              result.success = true;
+            }
+          }
+        } else {
+          result.isVerifyMFA = true;
+        }
+
+        if (result.isVerifyMFA || result.success || result.createCongregation) {
+          await handleUpdateSetting({ account_type: 'vip' });
+
+          if (result.isVerifyMFA) {
+            setCurrentMFAStage('verify');
+            setIsUserSignUp(false);
+            setUserMfaVerify(true);
+            setIsCongAccountCreate(false);
+            setIsUnauthorizedRole(false);
+          }
+
+          if (result.success) {
+            setIsSetup(false);
+
+            await runUpdater();
+
+            await setRootModalOpen(true);
+            const { status: scheduleStatus, data: scheduleData } = await apiFetchSchedule();
+            if (scheduleStatus === 200) {
+              await handleUpdateScheduleFromRemote(scheduleData);
+            }
+            await setRootModalOpen(false);
+
+            setTimeout(() => {
+              setCongAccountConnected(true);
+              setIsAppLoad(false);
+            }, [2000]);
+          }
+
+          if (result.createCongregation) {
+            setIsUserSignUp(false);
+            setIsUserSignIn(false);
+            setIsCongAccountCreate(true);
+          }
+        }
+
+        if (result.unauthorized) {
+          setIsUserSignUp(false);
+          setUserMfaVerify(true);
+          setIsCongAccountCreate(false);
+          setIsUnauthorizedRole(true);
+        }
+
+        setIsAuthProcessing(false);
+      }
     } catch (error) {
       await displayOnboardingFeedback({
         title: t('errorTitle'),
@@ -31,6 +143,8 @@ const useButtonBase = ({ provider, isEmail }) => {
       });
 
       showMessage();
+
+      setIsAuthProcessing(false);
     }
   };
 
--- src/v3/features/app_start/vip/startup/useStartup.jsx
+++ src/v3/features/app_start/vip/startup/useStartup.jsx
@@ -6,42 +6,25 @@ import {
   isCongAccountCreateState,
   isEmailAuthState,
   isEmailLinkAuthenticateState,
-  isOnlineState,
   isUserMfaVerifyState,
   isUserSignInState,
-  isUserSignUpState,
   offlineOverrideState,
-  visitorIDState,
 } from '@states/app';
 import {
-  displaySnackNotification,
-  setCongAccountConnected,
-  setCurrentMFAStage,
   setIsAppLoad,
-  setIsAuthProcessing,
   setIsCongAccountCreate,
   setIsEmailLinkAuthenticate,
   setIsSetup,
-  setIsUnauthorizedRole,
   setIsUserSignIn,
   setIsUserSignUp,
-  setOfflineOverride,
-  setRootModalOpen,
   setShowTermsUse,
   setUserMfaSetup,
   setUserMfaVerify,
 } from '@services/recoil/app';
 import { congNameState, congRoleState } from '@states/settings';
 import { CPE_ROLES } from '@constants/index';
-import { loadApp, runUpdater, updateUserInfoAfterLogin } from '@services/cpe';
-import { apiSendAuthorization } from '@services/api/user';
-import { handleUpdateSetting } from '@services/dexie/settings';
-import { apiFetchSchedule } from '@services/api/schedule';
-import { handleUpdateScheduleFromRemote } from '@services/cpe/schedules';
+import { loadApp, runUpdater } from '@services/cpe';
 import { convertStringToBoolean } from '@utils/common';
-import { getMessageByCode } from '@services/i18n/translation';
-
-window.userLoginRan = false;
 
 const useStartup = () => {
   const { isAuthenticated } = useFirebaseAuth();
@@ -50,9 +33,6 @@ const useStartup = () => {
 
   const isEmailLinkAuth = useRecoilValue(isEmailLinkAuthenticateState);
   const isEmailAuth = useRecoilValue(isEmailAuthState);
-  const isOnline = useRecoilValue(isOnlineState);
-  const visitorID = useRecoilValue(visitorIDState);
-  const isUserSignUp = useRecoilValue(isUserSignUpState);
   const isUserSignIn = useRecoilValue(isUserSignInState);
   const isUserMfaVerify = useRecoilValue(isUserMfaVerifyState);
   const isCongAccountCreate = useRecoilValue(isCongAccountCreateState);
@@ -67,123 +47,6 @@ const useStartup = () => {
     setUserMfaVerify(false);
     setUserMfaSetup(false);
   }, []);
-
-  const runAuthenticatedStep = useCallback(async () => {
-    try {
-      setIsAuthProcessing(true);
-
-      const approvedRole = congRole.some((role) => CPE_ROLES.includes(role));
-
-      if (!isOfflineOverride && congName.length > 0 && approvedRole) {
-        setIsSetup(false);
-        await loadApp();
-        await runUpdater();
-        setTimeout(() => {
-          setIsSetup(false);
-          setIsAppLoad(false);
-        }, [1000]);
-        return;
-      }
-
-      if (!window.userLoginRan) {
-        window.userLoginRan = true;
-
-        const { status, data } = await apiSendAuthorization();
-
-        if (status !== 200) {
-          await displaySnackNotification({
-            message: getMessageByCode(data.message),
-            severity: 'warning',
-          });
-
-          setIsAuthProcessing(false);
-          return;
-        }
-
-        const result = {};
-        const { cong_name, cong_role, mfa } = data;
-
-        if (mfa === 'not_enabled') {
-          if (cong_name.length === 0) {
-            result.createCongregation = true;
-          }
-
-          if (cong_name.length > 0 && cong_role.length === 0) {
-            result.unauthorized = true;
-          }
-
-          if (cong_name.length > 0 && cong_role.length > 0) {
-            const approvedRole = cong_role.some((role) => CPE_ROLES.includes(role));
-
-            if (!approvedRole) {
-              result.unauthorized = true;
-            }
-
-            if (approvedRole) {
-              await updateUserInfoAfterLogin(data);
-
-              result.success = true;
-            }
-          }
-        } else {
-          result.isVerifyMFA = true;
-        }
-
-        if (result.isVerifyMFA || result.success || result.createCongregation) {
-          await handleUpdateSetting({ account_type: 'vip' });
-
-          if (result.isVerifyMFA) {
-            setCurrentMFAStage('verify');
-            setIsUserSignUp(false);
-            setUserMfaVerify(true);
-            setIsCongAccountCreate(false);
-            setIsUnauthorizedRole(false);
-          }
-
-          if (result.success) {
-            setIsSetup(false);
-
-            await runUpdater();
-
-            await setRootModalOpen(true);
-            const { status: scheduleStatus, data: scheduleData } = await apiFetchSchedule();
-            if (scheduleStatus === 200) {
-              await handleUpdateScheduleFromRemote(scheduleData);
-            }
-            await setRootModalOpen(false);
-
-            setTimeout(() => {
-              setOfflineOverride(false);
-              setCongAccountConnected(true);
-              setIsAppLoad(false);
-            }, [2000]);
-          }
-
-          if (result.createCongregation) {
-            setIsUserSignUp(false);
-            setIsUserSignIn(false);
-            setIsCongAccountCreate(true);
-          }
-        }
-
-        if (result.unauthorized) {
-          setIsUserSignUp(false);
-          setUserMfaVerify(true);
-          setIsCongAccountCreate(false);
-          setIsUnauthorizedRole(true);
-        }
-
-        setIsAuthProcessing(false);
-      }
-    } catch (err) {
-      await displaySnackNotification({
-        message: getMessageByCode(err.message),
-        severity: 'error',
-      });
-
-      setIsAuthProcessing(false);
-    }
-  }, [isOfflineOverride, congName, congRole]);
 
   const runNotAuthenticatedStep = useCallback(async () => {
     if (isOfflineOverride) {
@@ -226,16 +89,6 @@ const useStartup = () => {
   useEffect(() => {
     if (!isAuthenticated) runNotAuthenticatedStep();
   }, [isAuthenticated, runNotAuthenticatedStep]);
-
-  useEffect(() => {
-    if (visitorID.toString().length === 0) return;
-
-    if (isUserSignUp || isUserSignIn) {
-      if (isAuthenticated) {
-        runAuthenticatedStep();
-      }
-    }
-  }, [isUserSignUp, isUserSignIn, runAuthenticatedStep, visitorID, isAuthenticated, isOnline]);
 
   useEffect(() => {
     const checkTermsUseStatus = async () => {
