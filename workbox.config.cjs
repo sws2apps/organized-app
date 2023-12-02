ffca7d80e9b400192e10f638904592ebd9a85ef0
rhahao <26148770+rhahao@users.noreply.github.com>
2023-12-02 13:21:43 +0300

    update workbox config

--- workbox.config.cjs
+++ workbox.config.cjs
@@ -1,8 +1,9 @@
+// eslint-disable-next-line no-undef
 module.exports = {
-	globDirectory: 'dist/',
-	globPatterns: ['**/*.{ico,json,png,html,js,css,webmanifest,pdf,svg,woff,woff2,ttf}'],
-	ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
-	swDest: 'dist/service-worker.js',
-	maximumFileSizeToCacheInBytes: 10000000,
-	sourcemap: false,
+  globDirectory: 'dist/',
+  globPatterns: ['**/*.{ico,json,png,html,js,css,webmanifest,pdf,svg,woff,woff2,ttf}'],
+  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/, /^v$/],
+  swDest: 'dist/service-worker.js',
+  maximumFileSizeToCacheInBytes: 10000000,
+  sourcemap: false,
 };
