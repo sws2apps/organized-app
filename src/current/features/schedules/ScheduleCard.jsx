21092739f33b60f42ec2f4aacf5d4cf9654c66aa
rhahao <26148770+rhahao@users.noreply.github.com>
2023-12-02 15:00:38 +0300

    merge from main

--- src/current/features/schedules/ScheduleCard.jsx
+++ src/current/features/schedules/ScheduleCard.jsx
@@ -27,7 +27,6 @@ import { Schedules } from '../../classes/Schedules';
 
 const ScheduleCard = ({ schedule }) => {
   const navigate = useNavigate();
-  const schedYear = +schedule.value.split('/')[1];
 
   const { t } = useTranslation('ui');
 
@@ -170,17 +169,9 @@ const ScheduleCard = ({ schedule }) => {
           open={openPrint}
           onClose={handleClosePrint}
         >
-<<<<<<< HEAD
-          {schedYear < 2024 && (
-            <MenuItem onClick={handleS89Export}>
-              <ListItemText primary={t('s89')} />
-            </MenuItem>
-          )}
-=======
           <MenuItem onClick={handleS89Export}>
             <ListItemText primary={t('s89')} />
           </MenuItem>
->>>>>>> main
 
           <MenuItem onClick={handleS140Export}>
             <ListItemText primary={t('midweekMeetingPrint')} />
