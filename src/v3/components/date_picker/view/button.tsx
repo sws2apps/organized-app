dc23fcbb331f8ec44a28fc9cfb97ffc56d21beb3
eloyEntony <kozyar.ant@gmail.com>
2024-02-07 11:00:44 +0200

    fix(schudules): eslint warning in date picker button view

--- src/v3/components/date_picker/view/button.tsx
+++ src/v3/components/date_picker/view/button.tsx
@@ -3,7 +3,14 @@ import { BaseSingleInputFieldProps, FieldSection } from '@mui/x-date-pickers';
 import { IconDate } from '@icons';
 import { format } from 'date-fns';
 import { Button } from '@mui/material';
-const ButtonField = ({
+import { FC } from 'react';
+
+interface ButtonFieldProps extends BaseSingleInputFieldProps<never, never, FieldSection, never> {
+  setOpen?: React.Dispatch<React.SetStateAction<boolean>>;
+  formatView: string;
+}
+
+const ButtonField: FC<ButtonFieldProps> = ({
   setOpen,
   id,
   value,
@@ -11,12 +18,7 @@ const ButtonField = ({
   InputProps: { ref } = {},
   inputProps: { 'aria-label': ariaLabel } = {},
   formatView,
-}: { setOpen?: React.Dispatch<React.SetStateAction<boolean>>; formatView: string } & BaseSingleInputFieldProps<
-  any,
-  any,
-  FieldSection,
-  any
->) => {
+}) => {
   return (
     <Button
       variant="text"
