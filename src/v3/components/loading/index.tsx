c689ecd9bdf98f241a30424cace1161e792f5eea
yrakhwas <yrakhwas@gmail.com>
2024-02-24 21:53:08 +0200

    Update type of translate component

--- src/v3/components/loading/index.tsx
+++ src/v3/components/loading/index.tsx
@@ -2,17 +2,17 @@ import { useAppTranslation } from '@hooks/index';
 import React from 'react';
 import IconLoading from '../icons/IconLoading';
 import { StyledCircleBox } from './loading.styles';
-import { Typography } from '@components';
+import Typography from '@components/typography';
 
 export const Loading = () => {
   const { t } = useAppTranslation();
 
   return (
-    <StyledCircleBox className='pop-up-shadow'>
-      <IconLoading color='var(--accent-main)' width={72} height={72}/>
-        <Typography className='h4' color='var(--accent-main)'>
-          {t('tr_loading')}
-        </Typography>
+    <StyledCircleBox className="pop-up-shadow">
+      <IconLoading color="var(--accent-main)" width={72} height={72} />
+      <Typography className="h4" color="var(--accent-main)">
+        {t('tr_loading')}
+      </Typography>
     </StyledCircleBox>
   );
 };
