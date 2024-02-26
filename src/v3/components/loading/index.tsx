ed7b260bd421125364b6fab72066195486457116
Yurii <101975674+yrakhwas@users.noreply.github.com>
2024-02-26 20:23:53 +0200

    adjust styles for loading circle

--- src/v3/components/loading/index.tsx
+++ src/v3/components/loading/index.tsx
@@ -1,35 +1,19 @@
-import Box from '@mui/material/Box';
-import IconLoading from '../icons/IconLoading';
-import Typography from '@components/typography';
 import { useAppTranslation } from '@hooks/index';
+import React from 'react';
+import IconLoading from '../icons/IconLoading';
+import { StyledCircleBox } from './loading.styles';
+import Typography from '@components/typography';
 
-const Loading = () => {
+export const Loading = () => {
   const { t } = useAppTranslation();
 
-  const boxStyles = {
-    height: '156px',
-    width: '156px',
-    backgroundColor: 'var(--always-white-base)',
-    borderRadius: 'var(--radius-xxl, 16px)',
-    display: 'flex',
-    padding: '24px 40px',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    boxShadow: '0px 16px 24px 0px rgba(28, 28, 28, 0.16)',
-  };
-  const circleLoading = {
-    width: '72px',
-    height: '72px',
-  };
-
   return (
-    <Box sx={boxStyles}>
-      <IconLoading sx={circleLoading} color="var(--accent-main)" />
-      <Typography variant="h4" color={'var(--accent-main)'} textAlign={'center'}>
-        {t('Loading...')}
+    <StyledCircleBox className="pop-up-shadow">
+      <IconLoading color="var(--accent-main)" width={72} height={72} />
+      <Typography className="h4" color="var(--accent-main)">
+        {t('tr_loading')}
       </Typography>
-    </Box>
+    </StyledCircleBox>
   );
 };
 
