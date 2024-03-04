99f478ddcbdedb7176b2cfee940bf9f99320bd22
rhahao <26148770+rhahao@users.noreply.github.com>
2024-03-04 17:15:21 +0300

    fix(icons): update converter script

--- src/v3/components/icons/IconDashedLine.tsx
+++ src/v3/components/icons/IconDashedLine.tsx
@@ -25,9 +25,9 @@ const IconDashedLine = ({ color = '#222222', width = 24, height = 24, sx = {} }:
         <g mask="url(#mask0_4944_2979629)">
           <path
             d="M4.58691 5.08747C4.58691 5.08747 11.4187 5.23278 15.4159 9.59343C19.4131 13.9541 19.4131 19.9136 19.4131 19.9136"
-            stroke="#222222"
-            stroke-width="1.5"
-            stroke-dasharray="3 2"
+            stroke={color}
+            strokeWidth="1.5"
+            strokeDasharray="3 2"
           />
         </g>
       </svg>
