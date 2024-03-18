a4820e3f477263b944f870beeb1845e7f258e894
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2024-03-18 20:56:13 +0300

    feat(pages): public talks list view (#1266)

--- src/v3/pages/meeting_materials/public_talks_list/index.tsx
+++ src/v3/pages/meeting_materials/public_talks_list/index.tsx
@@ -1,16 +1,19 @@
 import { Box } from '@mui/material';
+import { IconInfo, IconNoConnection, IconSpreadsheet, IconSync } from '@components/icons';
 import Button from '@components/button';
+import InfoTip from '@components/info_tip';
 import PageTitle from '@components/page_title';
 import TextMarkup from '@components/text_markup';
 import Typography from '@components/typography';
-import { useAppTranslation } from '@hooks/index';
-import { IconNoConnection, IconSync } from '@components/icons';
+import WaitingCircular from '@components/waiting_circular';
 import usePublicTalksList from './usePublicTalksList';
+import { useAppTranslation } from '@hooks/index';
+import { PublicTalks } from '@features/index';
 
 const PublicTalksList = () => {
   const { t } = useAppTranslation();
 
-  const { isConnected } = usePublicTalksList();
+  const { isConnected, talksList, handleSyncTalks, isFetching } = usePublicTalksList();
 
   return (
     <Box sx={{ display: 'flex', gap: '16px', flexDirection: 'column' }}>
@@ -19,40 +22,71 @@ const PublicTalksList = () => {
         backTo="/"
         buttons={
           isConnected ? (
-            <Button
-              variant="main"
-              startIcon={<IconSync height={22} width={22} color="var(--always-white)" />}
-              onClick={null}
-            >
-              {t('tr_syncTalksList')}
-            </Button>
+            <>
+              {talksList.length > 0 && (
+                <Button
+                  variant="secondary"
+                  startIcon={<IconSpreadsheet height={22} width={22} color="var(--accent-main)" />}
+                  disabled={isFetching}
+                >
+                  {t('tr_tableView')}
+                </Button>
+              )}
+
+              <Button
+                variant="main"
+                startIcon={<IconSync height={22} width={22} color="var(--always-white)" />}
+                onClick={handleSyncTalks}
+                disabled={isFetching}
+              >
+                {t('tr_syncTalksList')}
+              </Button>
+            </>
           ) : null
         }
       />
 
-      {!isConnected && (
-        <Box
-          sx={{
-            height: 'calc(100vh - 160px)',
-            display: 'flex',
+      {isFetching && <WaitingCircular />}
 
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}
-        >
-          <Box sx={{ maxWidth: '702px', display: 'flex', alignItems: 'center', gap: '24px' }}>
-            <IconNoConnection height={74} width={74} color="var(--red-main)" />
-            <Box sx={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
-              <Typography className="h1">{t('tr_offline')}</Typography>
-              <TextMarkup
-                content={t('tr_publicTalkOfflineDesc')}
-                className="h4"
-                color="var(--grey-400)"
-                anchorClassName="body-regular"
-              />
+      {!isFetching && (
+        <>
+          {isConnected && talksList.length === 0 && (
+            <InfoTip
+              isBig={false}
+              text={t('tr_noTalksList')}
+              icon={<IconInfo />}
+              color="white"
+              sx={{ maxWidth: '700px' }}
+            />
+          )}
+
+          {talksList.length > 0 && <PublicTalks />}
+
+          {!isConnected && talksList.length === 0 && (
+            <Box
+              sx={{
+                height: 'calc(100vh - 200px)',
+                display: 'flex',
+
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}
+            >
+              <Box sx={{ maxWidth: '702px', display: 'flex', alignItems: 'center', gap: '24px' }}>
+                <IconNoConnection height={74} width={74} color="var(--red-main)" />
+                <Box sx={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
+                  <Typography className="h1">{t('tr_offline')}</Typography>
+                  <TextMarkup
+                    content={t('tr_publicTalkOfflineDesc')}
+                    className="h4"
+                    color="var(--grey-400)"
+                    anchorClassName="body-regular"
+                  />
+                </Box>
+              </Box>
             </Box>
-          </Box>
-        </Box>
+          )}
+        </>
       )}
     </Box>
   );
