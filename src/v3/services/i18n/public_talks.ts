e9e79a6f9cca36a4509f49729c534bdb7db987ee
rhahao <rhahao.vj@gmail.com>
2024-04-15 06:52:52 +0300

    git update

--- src/v3/services/i18n/public_talks.ts
+++ src/v3/services/i18n/public_talks.ts
@@ -1,6 +1,18 @@
 import { getI18n } from 'react-i18next';
+import { PublicTalkType } from '@definition/public_talks';
 
 export const publicTalksBuildList = (language: string) => {
-  const talks = getI18n().options.resources[language].talks;
-  console.log(talks);
+  const translations = getI18n().options.resources[language].talks;
+
+  const result: PublicTalkType[] = [];
+  for (const [key, value] of Object.entries(translations)) {
+    const number = +key.split('_')[2];
+
+    result.push({
+      talk_number: number,
+      talk_title: value,
+    });
+  }
+
+  return result;
 };
