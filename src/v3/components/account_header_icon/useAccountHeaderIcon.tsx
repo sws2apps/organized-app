bf991f45fa3ee925420fb0cca55d73e2d21dc1d4
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2024-04-20 21:30:23 +0300

    fix: suspense for routes and type update for user_avatar data (#1575)

--- src/v3/components/account_header_icon/useAccountHeaderIcon.tsx
+++ src/v3/components/account_header_icon/useAccountHeaderIcon.tsx
@@ -1,13 +1,13 @@
 import { useRecoilValue } from 'recoil';
 import { congAccountConnectedState, isAppLoadState } from '@states/app';
-import { userAvatarState } from '@states/settings';
+import { userAvatarUrlState } from '@states/settings';
 
 /**
  * Custom hook for managing the user's avatar and offline status.
  * @returns {Object} Object containing userAvatar and isOffline properties
  */
 export const useAccountHeaderIcon = () => {
-  const userAvatar = useRecoilValue(userAvatarState);
+  const userAvatar = useRecoilValue(userAvatarUrlState);
   const congAccountConnected = useRecoilValue(congAccountConnectedState);
   const isAppLoad = useRecoilValue(isAppLoadState);
 
