1ff2c28f7c761d5bf672cbde24bfaacdf73ebe2b
rhahao <26148770+rhahao@users.noreply.github.com>
2024-04-21 22:57:40 +0300

    update congregation field service table

--- src/shared/indexedDb/tables/cong_field_service_reports.ts
+++ src/shared/indexedDb/tables/cong_field_service_reports.ts
@@ -1,11 +1,11 @@
 import { Table } from 'dexie';
-import { UserFieldServiceReportType } from '@definition/user_field_service_reports';
+import { CongFieldServiceReportType } from '@definition/cong_field_service_reports';
 
 export type CongFieldServiceReportsTable = {
-  cong_field_service_reports: Table<UserFieldServiceReportType>;
+  cong_field_service_reports: Table<CongFieldServiceReportType>;
 };
 
 export const congFieldServiceReportsSchema = {
   cong_field_service_reports:
-    '&id, _deleted, person_uid, month_date, duration, hours_credits, bible_studies, comments, posted_date',
+    '&id, _deleted, person_uid, month_date, shared_ministry, hours, hours_credits, bible_studies, comments, posted_date',
 };
