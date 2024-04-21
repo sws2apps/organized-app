c996dea41ad75348511d2db3d1d028251fd904be
rhahao <26148770+rhahao@users.noreply.github.com>
2024-04-21 22:54:29 +0300

    update meeting attendance type

--- src/v3/definition/meeting_attendance.ts
+++ src/v3/definition/meeting_attendance.ts
@@ -1,24 +1,14 @@
+type WeeklyAttendance = {
+  midweek: { present: number; online: number };
+  weekend: { present: number; online: number };
+};
+
 export type MeetingAttendanceType = {
   id?: string;
   month_date: string;
-  week_1: {
-    midweek: { present: number; online: number };
-    weekend: { present: number; online: number };
-  };
-  week_2: {
-    midweek: { present: number; online: number };
-    weekend: { present: number; online: number };
-  };
-  week_3: {
-    midweek: { present: number; online: number };
-    weekend: { present: number; online: number };
-  };
-  week_4: {
-    midweek: { present: number; online: number };
-    weekend: { present: number; online: number };
-  };
-  week_5: {
-    midweek: { present: number; online: number };
-    weekend: { present: number; online: number };
-  };
+  week_1: WeeklyAttendance;
+  week_2: WeeklyAttendance;
+  week_3: WeeklyAttendance;
+  week_4: WeeklyAttendance;
+  week_5: WeeklyAttendance;
 };
