77e6ce2a35cde2406f62388639f4124de7d1c55c
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2024-04-21 23:47:21 +0300

    feat(persons): add new illustrations for empty record and search results (#1594)

--- src/v3/features/persons/list/index.tsx
+++ src/v3/features/persons/list/index.tsx
@@ -2,6 +2,7 @@ import { Box } from '@mui/material';
 import { useAppTranslation } from '@hooks/index';
 import PersonsListAll from './all_persons';
 import PersonsRecent from './recent_persons';
+import PersonsEmpty from './persons_empty';
 import Tabs from '@components/tabs';
 import Typography from '@components/typography';
 import useList from './useList';
@@ -9,26 +10,30 @@ import useList from './useList';
 const PersonsList = () => {
   const { t } = useAppTranslation();
 
-  const { persons, activeTab, handleTabChange } = useList();
+  const { persons, activeTab, handleTabChange, personsAll } = useList();
 
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', gap: '16px', marginTop: '16px' }}>
       <Typography className="h2">{t('tr_personsAmount', { amount: persons.length })}</Typography>
 
-      <Tabs
-        value={activeTab}
-        onChange={handleTabChange}
-        tabs={[
-          {
-            label: t('tr_personsAll'),
-            Component: <PersonsListAll />,
-          },
-          {
-            label: t('tr_recentlyViewed'),
-            Component: <PersonsRecent />,
-          },
-        ]}
-      />
+      {personsAll.length === 0 && <PersonsEmpty />}
+
+      {personsAll.length > 0 && (
+        <Tabs
+          value={activeTab}
+          onChange={handleTabChange}
+          tabs={[
+            {
+              label: t('tr_personsAll'),
+              Component: <PersonsListAll />,
+            },
+            {
+              label: t('tr_recentlyViewed'),
+              Component: <PersonsRecent />,
+            },
+          ]}
+        />
+      )}
     </Box>
   );
 };
