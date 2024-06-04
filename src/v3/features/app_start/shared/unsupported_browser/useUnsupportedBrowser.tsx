a0244feb3630636b664f55840ce1ad22ab0c1886
velthereal <lera2005olex@gmail.com>
2024-06-04 11:07:02 +0300

    fixed added useCallback in useUnsupportedBrowser file

--- src/v3/features/app_start/shared/unsupported_browser/useUnsupportedBrowser.tsx
+++ src/v3/features/app_start/shared/unsupported_browser/useUnsupportedBrowser.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 
 const useUnsupportedBrowser = () => {
   const anchorRef = useRef<HTMLElement>();
 
-  const reloadApp = () => {
+  const reloadApp = useCallback(() => {
     window.location.reload();
-  };
+  }, []);
 
   useEffect(() => {
     const tryAgain = anchorRef.current;
