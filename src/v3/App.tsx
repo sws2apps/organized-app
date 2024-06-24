eeddd4d687df7f85b9f2b84969adacc2f53e5234
rhahao <26148770+rhahao@users.noreply.github.com>
2024-06-24 21:50:25 +0300

    fix(app): connect unsupported browser component

--- src/v3/App.tsx
+++ src/v3/App.tsx
@@ -2,7 +2,6 @@ import { Suspense, lazy } from 'react';
 import { RouterProvider, createHashRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ErrorBoundary, WaitingCircular } from '@components/index';
-import { useGlobal } from '@hooks/index';
 import { RootLayout } from '@layouts/index';
 
 // lazy loading
@@ -24,8 +23,6 @@ const PdfPreview = lazy(() => import('@components/preview/PDF_Peview'));
 const queryClient = new QueryClient();
 
 const App = ({ updatePwa }: { updatePwa: VoidFunction }) => {
-  const { isSupported } = useGlobal();
-
   const router = createHashRouter([
     {
       errorElement: <ErrorBoundary />,
@@ -58,22 +55,11 @@ const App = ({ updatePwa }: { updatePwa: VoidFunction }) => {
   ]);
 
   return (
-    <>
-      {!isSupported && (
-        <div className="browser-not-supported">
-          You are using unsupported browser for the Congregation Program for Everyone app. Make sure that your browser
-          is up to date, or try to use another browser.
-        </div>
-      )}
-
-      {isSupported && (
-        <QueryClientProvider client={queryClient}>
-          <Suspense fallback={<WaitingCircular />}>
-            <RouterProvider router={router} />
-          </Suspense>
-        </QueryClientProvider>
-      )}
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Suspense fallback={<WaitingCircular />}>
+        <RouterProvider router={router} />
+      </Suspense>
+    </QueryClientProvider>
   );
 };
 export default App;
--- src/v3/features/app_start/shared/unsupported_browser/useUnsupportedBrowser.tsx
+++ src/v3/features/app_start/shared/unsupported_browser/useUnsupportedBrowser.tsx
@@ -1,23 +1,7 @@
-import { useCallback, useEffect, useRef } from 'react';
-
 const useUnsupportedBrowser = () => {
-  const anchorRef = useRef<HTMLElement>();
+  const reloadApp = () => window.location.reload();
 
-  const reloadApp = useCallback(() => {
-    window.location.reload();
-  }, []);
-
-  useEffect(() => {
-    const tryAgain = anchorRef.current;
-
-    tryAgain?.addEventListener('click', reloadApp);
-
-    return () => {
-      if (tryAgain) tryAgain?.removeEventListener('click', reloadApp);
-    };
-  }, [reloadApp]);
-
-  return { anchorRef, reloadApp };
+  return { reloadApp };
 };
 
 export default useUnsupportedBrowser;
