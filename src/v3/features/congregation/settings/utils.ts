32f7cf1135ad4d520439b4db308fae002057dfdf
Alonzo <77450724+Al11o@users.noreply.github.com>
2024-07-25 17:29:21 +0200

    Cu 86bwch8q0 congregation settings page alonzo pineau (#2131)
    Co-authored-by: Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>

--- src/v3/features/congregation/settings/utils.ts
+++ src/v3/features/congregation/settings/utils.ts
@@ -16,4 +16,46 @@ const handleNumber: HandleNumbers = (e, setFunction) => {
   setFunction(Number(e.target.value.replaceAll(/\D/g, '')));
 };
 
-export { handleBoolean, handleNumber };
+const textFieldSelectStyles = {
+  SelectProps: {
+    MenuProps: {
+      sx: {
+        '.MuiPaper-root': {
+          backgroundColor: 'var(--white)',
+          borderColor: 'var(--accent-200)',
+          color: 'var(--black)',
+        },
+      },
+    },
+  },
+  sx: {
+    '.MuiOutlinedInput-root': {
+      borderRadius: 'var(--radius-l)',
+      paddingRight: '8px',
+      color: 'var(--black)',
+      '& svg': {
+        color: 'var(--accent-350)',
+      },
+      '&.Mui-focused svg': {
+        color: 'var(--black)',
+      },
+      '& fieldset': {
+        border: '1px solid var(--accent-350)',
+      },
+      '&:hover fieldset': {
+        border: '1px solid var(--accent-main)',
+      },
+      '&.Mui-focused fieldset': {
+        border: '1px solid var(--accent-main)',
+      },
+    },
+    '.MuiInputLabel-root': {
+      color: 'var(--accent-350)',
+      '&.Mui-focused': {
+        color: 'var(--accent-main)',
+      },
+    },
+  },
+};
+
+export { handleBoolean, handleNumber, textFieldSelectStyles };
