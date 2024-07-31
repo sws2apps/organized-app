4fd7532e7f0d6b24d20b1caacb1c3fcca5ed2d0d
Alonzo Pineau <alonzo.mail@icloud.com>
2024-08-01 00:39:49 +0200

    feat(publisher-records-details): left panel

--- src/v3/pages/congregation/publisher_records_detail/index.tsx
+++ src/v3/pages/congregation/publisher_records_detail/index.tsx
@@ -1,5 +1,44 @@
+import { IconExport } from '@components/icons';
+import { Button } from '@components/index';
+import PageTitle from '@components/page_title';
+import {
+  LeftColumn,
+  RightColumn,
+  Wrapper,
+} from '@features/congregation/publisher_records/Wrappers';
+import PublisherDetailCard from '@features/congregation/publisher_records_detail/PublisherDetailCard';
+import { CardSection } from '@features/congregation/settings/CardSection';
+import useAppTranslation from '@hooks/useAppTranslation';
+import { Box } from '@mui/material';
+
 const PublisherRecordDetail = () => {
-  return <div>Publisher Record details</div>;
+  const { t } = useAppTranslation();
+
+  const handleExport = () => {
+    console.log('Export S21');
+  };
+  return (
+    <>
+      <Box sx={{ display: 'flex', gap: '16px', flexDirection: 'column' }}>
+        <PageTitle
+          title={t('tr_publishersRecords')}
+          buttons={
+            <Button startIcon={<IconExport />} onClick={handleExport}>
+              {t('tr_exportS21')}
+            </Button>
+          }
+        />
+      </Box>
+      <Wrapper>
+        <LeftColumn>
+          <PublisherDetailCard />
+        </LeftColumn>
+        <RightColumn>
+          <CardSection></CardSection>
+        </RightColumn>
+      </Wrapper>
+    </>
+  );
 };
 
 export default PublisherRecordDetail;
