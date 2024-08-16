89bf3d72bb402d37c11bf7ad651b835aac259466
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2024-08-16 19:13:29 +0300

    feat(materials): jw.org materials auto-import (#2324)

---
+++ src/features/meeting_materials/jw_auto_import/index.tsx
@@ -0,0 +1,55 @@
+import { useCallback, useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
+import { isDemo } from '@constants/index';
+import { isAppLoadState } from '@states/app';
+import {
+  adminRoleState,
+  coordinatorRoleState,
+  lmmoRoleState,
+  publicTalkCoordinatorRoleState,
+  sourcesJWAutoImportState,
+} from '@states/settings';
+import { apiFetchSources } from '@services/api/sources';
+import { sourcesImportJW } from '@services/app/sources';
+
+const JWAutoImport = () => {
+  const isAppLoad = useRecoilValue(isAppLoadState);
+  const isAdmin = useRecoilValue(adminRoleState);
+  const isLMMO = useRecoilValue(lmmoRoleState);
+  const isCoordinator = useRecoilValue(coordinatorRoleState);
+  const isPublicTalkCoordinator = useRecoilValue(
+    publicTalkCoordinatorRoleState
+  );
+  const isAutoImportEnabled = useRecoilValue(sourcesJWAutoImportState);
+
+  const approvedRole =
+    isAdmin || isLMMO || isCoordinator || isPublicTalkCoordinator;
+
+  const handleJWAutoImport = useCallback(async () => {
+    const { data, status } = await apiFetchSources();
+    if (status === 200 && data?.length) {
+      await sourcesImportJW(data);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!isAppLoad) {
+      if (isDemo) {
+        handleJWAutoImport();
+      }
+
+      if (!isDemo && isAutoImportEnabled && approvedRole) {
+        const now = new Date().toISOString();
+        const nextSync = localStorage.getItem('organized_jw-import-next-sync');
+
+        if (!nextSync || (nextSync && nextSync <= now)) {
+          handleJWAutoImport();
+        }
+      }
+    }
+  }, [handleJWAutoImport, isAppLoad, isAutoImportEnabled, approvedRole]);
+
+  return <></>;
+};
+
+export default JWAutoImport;
