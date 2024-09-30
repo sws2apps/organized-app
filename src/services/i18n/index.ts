29cbfa874498b9810345f415da604401609c3074
rhahao <26148770+rhahao@users.noreply.github.com>
2024-09-30 19:48:50 +0300

    fixing code smells

--- src/services/i18n/index.ts
+++ src/services/i18n/index.ts
@@ -11,13 +11,13 @@ const getAppLang = () => {
   const cookiesConsent = Boolean(localStorage.getItem('userConsent'));
 
   if (cookiesConsent) {
-    return localStorage.getItem('app_lang') || 'en';
+    return localStorage.getItem('app_lang')?.toString() || 'en';
   }
 
   const hash = new URL(window.location.href).hash;
   const params = new URLSearchParams(hash.substring(2));
 
-  return params.get('locale') || 'en';
+  return params.get('locale')?.toString() || 'en';
 };
 
 const appLang = getAppLang();
