dd39a9112e3616fc0e8805f65bba0df4266f455e
Max Makaluk <70341920+FussuChalice@users.noreply.github.com>
2024-10-28 12:47:08 +0200

    feat(person_item): add tooltips for report statuses

--- src/features/reports/field_service/persons_list/person_item/index.tsx
+++ src/features/reports/field_service/persons_list/person_item/index.tsx
@@ -7,10 +7,14 @@ import { UserCard } from './index.styles';
 import { PersonItemProps } from './index.types';
 import usePersonItem from './usePersonItem';
 import PersonDetails from '@features/persons/person_details';
+import { Tooltip } from '@components/index';
+import { useAppTranslation } from '@hooks/index';
 
 const PersonItem = (props: PersonItemProps) => {
   const { report_status, isSelected, handleToggleSelect, currentMonth } =
     usePersonItem(props);
+
+  const { t } = useAppTranslation();
 
   return (
     <UserCard
@@ -23,15 +27,21 @@ const PersonItem = (props: PersonItemProps) => {
       <PersonDetails person={props.person} month={currentMonth} />
 
       {report_status === 'confirmed' && (
-        <IconCheckmarkCircleAlt color="var(--accent-main)" />
+        <Tooltip label={t('tr_verified')} delaySpeed={'fast'} use>
+          <IconCheckmarkCircleAlt color="var(--accent-main)" />
+        </Tooltip>
       )}
 
       {report_status === 'received' && (
-        <IconReportReceived color="var(--accent-main)" />
+        <Tooltip label={t('tr_pendingVerification')} delaySpeed={'fast'} use>
+          <IconReportReceived color="var(--accent-main)" />
+        </Tooltip>
       )}
 
       {report_status === 'not_received' && (
-        <IconReportWaiting color="var(--accent-main)" />
+        <Tooltip label={t('tr_notSubmitted')} delaySpeed={'fast'} use>
+          <IconReportWaiting color="var(--accent-main)" />
+        </Tooltip>
       )}
     </UserCard>
   );
