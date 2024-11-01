cc913c6ec69fab07ed33d9498c5601ed265056f7
rhahao <26148770+rhahao@users.noreply.github.com>
2024-11-01 13:48:14 +0300

    fix(onboarding): dot navigation and add more slides

--- src/features/app_start/shared/illustration/index.tsx
+++ src/features/app_start/shared/illustration/index.tsx
@@ -4,22 +4,20 @@ import 'swiper/css/pagination';
 import { Box, IconButton } from '@mui/material';
 import { IconEllipse } from '@icons/index';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { useAppTranslation } from '@hooks/index';
 import { Autoplay, Pagination } from 'swiper/modules';
-import { createNumbersArray } from '@utils/common';
 import { SlideItem } from './index.styles';
 import useIllustration from './useIllustration';
-import MeetingSchedules from '@assets/img/illustration_meetingSchedules.svg?url';
-import MinistryAssignments from '@assets/img/illustration_ministryAssignments.svg?url';
-import MultiPlattform from '@assets/img/illustration_multiPlattform.svg?url';
-import Secretary from '@assets/img/illustration_secretary.svg?url';
 import Typography from '@components/typography';
 
 const StartupIllustration = () => {
-  const { t } = useAppTranslation();
-
-  const { index, setIndex, dotSize, handleSlide, swiperRef } =
-    useIllustration();
+  const {
+    currentImage,
+    setCurrentImage,
+    dotSize,
+    handleSlide,
+    swiperRef,
+    slides,
+  } = useIllustration();
 
   return (
     <Box
@@ -50,108 +48,34 @@ const StartupIllustration = () => {
           modules={[Autoplay, Pagination]}
           className="mySwiper"
           style={{ height: '100%' }}
-          onRealIndexChange={(swiper) => setIndex(swiper.realIndex)}
+          onRealIndexChange={(swiper) => setCurrentImage(swiper.realIndex)}
         >
-          <SwiperSlide>
-            <SlideItem>
-              <Box>
-                <Typography
-                  className="h1"
-                  color="var(--always-white)"
-                  sx={{ marginBottom: '24px' }}
-                >
-                  {t('tr_illustrationMinistryAssignmentsHeader')}
-                </Typography>
-                <Typography
-                  className="body-regular"
-                  color="var(--always-white)"
-                >
-                  {t('tr_illustrationMinistryAssignmentsDescription')}
-                </Typography>
-              </Box>
-              <img
-                alt=""
-                src={MinistryAssignments}
-                style={{ width: '100%', height: 'auto' }}
-              />
-            </SlideItem>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <SlideItem>
-              <Box>
-                <Typography
-                  className="h1"
-                  color="var(--always-white)"
-                  sx={{ marginBottom: '24px' }}
-                >
-                  {t('tr_illustrationMultiPlattformHeader')}
-                </Typography>
-                <Typography
-                  className="body-regular"
-                  color="var(--always-white)"
-                >
-                  {t('tr_illustrationMultiPlattformDescription')}
-                </Typography>
-              </Box>
-              <img
-                alt=""
-                src={MultiPlattform}
-                style={{ width: '100%', height: 'auto' }}
-              />
-            </SlideItem>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <SlideItem>
-              <Box>
-                <Typography
-                  className="h1"
-                  color="var(--always-white)"
-                  sx={{ marginBottom: '24px' }}
-                >
-                  {t('tr_illustrationMeetingSchedulesHeader')}
-                </Typography>
-                <Typography
-                  className="body-regular"
-                  color="var(--always-white)"
-                >
-                  {t('tr_illustrationMeetingSchedulesDescription')}
-                </Typography>
-              </Box>
-              <img
-                alt=""
-                src={MeetingSchedules}
-                style={{ width: '100%', height: 'auto' }}
-              />
-            </SlideItem>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <SlideItem>
-              <Box>
-                <Typography
-                  className="h1"
-                  color="var(--always-white)"
-                  sx={{ marginBottom: '24px' }}
-                >
-                  {t('tr_illustrationSecretaryHeader')}
-                </Typography>
-                <Typography
-                  className="body-regular"
-                  color="var(--always-white)"
-                >
-                  {t('tr_illustrationSecretaryDescription')}
-                </Typography>
-              </Box>
-
-              <img
-                alt=""
-                src={Secretary}
-                style={{ width: '100%', height: 'auto' }}
-              />
-            </SlideItem>
-          </SwiperSlide>
+          {slides.map((slide) => (
+            <SwiperSlide key={slide.title}>
+              <SlideItem>
+                <Box>
+                  <Typography
+                    className="h1"
+                    color="var(--always-white)"
+                    sx={{ marginBottom: '24px' }}
+                  >
+                    {slide.title}
+                  </Typography>
+                  <Typography
+                    className="body-regular"
+                    color="var(--always-white)"
+                  >
+                    {slide.desc}
+                  </Typography>
+                </Box>
+                <img
+                  alt=""
+                  src={slide.src}
+                  style={{ width: '100%', height: 'auto' }}
+                />
+              </SlideItem>
+            </SwiperSlide>
+          ))}
         </Swiper>
       </Box>
 
@@ -163,22 +87,24 @@ const StartupIllustration = () => {
           gap: { mobile: '12px', laptop: '16px' },
         }}
       >
-        {createNumbersArray(4).map((n) => (
+        {slides.map((item, index) => (
           <IconButton
-            key={n}
+            key={item.title}
             disableRipple
             sx={{
-              opacity: n === index ? 1 : 0.48,
+              opacity: currentImage === index ? 1 : 0.48,
               padding: 0,
               margin: 0,
               width: { mobile: '12px', laptop: '16px' },
             }}
-            onClick={() => handleSlide(n)}
+            onClick={() => handleSlide(index)}
           >
             <IconEllipse
               color="var(--always-white)"
-              width={n === index ? dotSize.active : dotSize.inactive}
-              height={n === index ? dotSize.active : dotSize.inactive}
+              width={currentImage === index ? dotSize.active : dotSize.inactive}
+              height={
+                currentImage === index ? dotSize.active : dotSize.inactive
+              }
             />
           </IconButton>
         ))}
