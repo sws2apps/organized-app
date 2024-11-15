09d1ce1985138fccbb1268a4f4a3499821eb0cfe
rhahao.vj <26148770+rhahao@users.noreply.github.com>
2024-11-15 07:32:53 +0000

    feat(meetings): add tooltip when selecting date range

--- src/features/meetings/schedule_range_selector/index.tsx
+++ src/features/meetings/schedule_range_selector/index.tsx
@@ -2,8 +2,9 @@ import { Box } from '@mui/material';
 import { useAppTranslation, useBreakpoints } from '@hooks/index';
 import { ScheduleRangeSelectorType } from './index.types';
 import useScheduleRangeSelector from './useScheduleRangeSelector';
-import Select from '@components/select';
 import MenuItem from '@components/menuitem';
+import Select from '@components/select';
+import Tooltip from '@components/tooltip';
 import Typography from '@components/typography';
 
 const ScheduleRangeSelector = ({
@@ -14,8 +15,12 @@ const ScheduleRangeSelector = ({
 
   const { tabletUp } = useBreakpoints();
 
-  const { endMonthOptions, handleStartMonthChange, startMonthOptions } =
-    useScheduleRangeSelector(onStartChange);
+  const {
+    endMonthOptions,
+    handleStartMonthChange,
+    startMonthOptions,
+    startMonth,
+  } = useScheduleRangeSelector(onStartChange);
 
   return (
     <Box
@@ -40,19 +45,26 @@ const ScheduleRangeSelector = ({
         ))}
       </Select>
 
-      <Select
-        label={t('tr_endMonth')}
-        defaultValue=""
-        onChange={(e) => onEndChange?.(e.target.value as string)}
+      <Tooltip
+        show={startMonth.length === 0}
+        title={t('tr_dateRangeSelectStart')}
+        sx={{ width: '100%' }}
       >
-        {endMonthOptions.map((option) => (
-          <MenuItem key={option.value} value={option.value}>
-            <Typography className="body-regular" color="var(--black)">
-              {option.label}
-            </Typography>
-          </MenuItem>
-        ))}
-      </Select>
+        <Select
+          label={t('tr_endMonth')}
+          defaultValue=""
+          disabled={startMonth.length === 0}
+          onChange={(e) => onEndChange?.(e.target.value as string)}
+        >
+          {endMonthOptions.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              <Typography className="body-regular" color="var(--black)">
+                {option.label}
+              </Typography>
+            </MenuItem>
+          ))}
+        </Select>
+      </Tooltip>
     </Box>
   );
 };
