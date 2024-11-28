2302bade83cbe1eb599b944a5fcec6c11e5fe26f
Max Makaluk <70341920+FussuChalice@users.noreply.github.com>
2024-11-28 14:29:13 +0200

    fix(app): Change divider color – public talk list

--- src/components/table/TableHead.tsx
+++ src/components/table/TableHead.tsx
@@ -42,7 +42,7 @@ const TableHead = (props: EnhancedTableProps) => {
             sortDirection={orderBy === headCell.id ? order : false}
             sx={{
               backgroundColor: 'var(--accent-100)',
-              borderColor: 'var(--accent-200)',
+              borderColor: 'rgba(224, 224, 224, 1);',
               ...headCell.sx,
             }}
           >
