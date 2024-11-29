d3eeab6722937c746dfa019012f46cf040da56d4
Max Makaluk <70341920+FussuChalice@users.noreply.github.com>
2024-11-29 09:03:10 +0200

    feat(app): update UI and fix bugs for credit hours and assignment restrictions

--- src/features/persons/assignment_group/index.tsx
+++ src/features/persons/assignment_group/index.tsx
@@ -2,6 +2,7 @@ import AssignmentsCheckList from '@components/assignments_checklist';
 import Checkbox from '@components/checkbox';
 import { AssignmentGroupType } from './index.types';
 import useAssignmentGroup from './useAssignmentGroup';
+import Tooltip from '@components/tooltip';
 
 const AssignmentGroup = ({
   header,
@@ -15,42 +16,57 @@ const AssignmentGroup = ({
   disqualified = false,
   readOnly,
 }: AssignmentGroupType) => {
-  const { checkAssignmentDisabled, checkGroupDisabled } =
-    useAssignmentGroup(male);
+  const {
+    checkAssignmentDisabled,
+    checkGroupDisabled,
+    isMinistryDisabled,
+    isDisabledByGender,
+    getTooltipsForAssignmentTitles,
+  } = useAssignmentGroup(male);
 
   return (
-    <AssignmentsCheckList
-      header={header}
-      color={color}
-      disabled={disqualified || checkGroupDisabled(id)}
-      onChange={(checked) => onHeaderChange(checked, id)}
-      readOnly={readOnly}
+    <Tooltip
+      followCursor
+      title={getTooltipsForAssignmentTitles(id, items)}
+      show={isMinistryDisabled(id, items) || isDisabledByGender(id)}
     >
-      {items.map((assignment) => (
-        <Checkbox
-          key={assignment.code}
-          readOnly={readOnly}
-          label={assignment.name}
-          checked={
-            checkAssignmentDisabled(assignment.code)
-              ? false
-              : checkedItems.includes(assignment.code)
-          }
-          onChange={(_, checked) => onItemChange(checked, assignment.code)}
-          className="body-small-regular"
-          disabled={disqualified || checkAssignmentDisabled(assignment.code)}
-          sx={
-            assignment.borderTop
-              ? {
-                  borderTop: '1px solid var(--accent-200)',
-                  marginTop: '4px',
-                  paddingTop: '8px',
-                }
-              : {}
-          }
-        />
-      ))}
-    </AssignmentsCheckList>
+      <AssignmentsCheckList
+        header={header}
+        color={color}
+        disabled={
+          disqualified ||
+          checkGroupDisabled(id) ||
+          isMinistryDisabled(id, items)
+        }
+        onChange={(checked) => onHeaderChange(checked, id)}
+        readOnly={readOnly}
+      >
+        {items.map((assignment) => (
+          <Checkbox
+            key={assignment.code}
+            readOnly={readOnly}
+            label={assignment.name}
+            checked={
+              checkAssignmentDisabled(assignment.code)
+                ? false
+                : checkedItems.includes(assignment.code)
+            }
+            onChange={(_, checked) => onItemChange(checked, assignment.code)}
+            className="body-small-regular"
+            disabled={disqualified || checkAssignmentDisabled(assignment.code)}
+            sx={
+              assignment.borderTop
+                ? {
+                    borderTop: '1px solid var(--accent-200)',
+                    marginTop: '4px',
+                    paddingTop: '8px',
+                  }
+                : {}
+            }
+          />
+        ))}
+      </AssignmentsCheckList>
+    </Tooltip>
   );
 };
 
