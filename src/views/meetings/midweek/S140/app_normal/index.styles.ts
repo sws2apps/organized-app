c12ce87435b66c702e86ada4400953c1d581b23a
Stas <131067974+mrtavor@users.noreply.github.com>
2024-11-29 14:50:19 +0100

    Edited the weekContainer gap to 24px.

--- src/views/meetings/midweek/S140/app_normal/index.styles.ts
+++ src/views/meetings/midweek/S140/app_normal/index.styles.ts
@@ -25,7 +25,7 @@ const styles = StyleSheet.create({
     fontSize: '12px',
   },
   weekContainer: {
-    marginBottom: '5px',
+    marginBottom: '24px',
   },
   weekHeader: {
     padding: '4px 5px 4px 15px',
