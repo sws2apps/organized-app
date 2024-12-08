04823d9d71a9c25bcfc91bc12896e32d1c05568c
rhahao <26148770+rhahao@users.noreply.github.com>
2024-12-08 14:24:09 +0300

    fix(locales): issue with tagalog language translation

--- src/features/whats_new/useWhatsNew.tsx
+++ src/features/whats_new/useWhatsNew.tsx
@@ -3,7 +3,7 @@ import { SwiperRef } from 'swiper/react';
 import { useRecoilValue } from 'recoil';
 import { useAppTranslation } from '@hooks/index';
 import { ReleaseNoteType, UpdateStatusType } from '@definition/app';
-import { isDemo } from '@constants/index';
+import { isDemo, LANGUAGE_LIST } from '@constants/index';
 import { ImageSlide } from './index.types';
 import { appLangState } from '@states/app';
 
@@ -22,9 +22,16 @@ const useWhatsNew = () => {
   const [improvements, setImprovements] = useState<string[]>([]);
   const [currentImage, setCurrentImage] = useState(0);
 
+  const identifier = useMemo(() => {
+    return (
+      LANGUAGE_LIST.find((record) => record.locale === appLang)?.identifier ||
+      appLang
+    );
+  }, [appLang]);
+
   const releases = useMemo(() => {
-    return i18n.options.resources[appLang].releases as ReleaseNoteType;
-  }, [appLang, i18n]);
+    return i18n.options.resources[identifier].releases as ReleaseNoteType;
+  }, [identifier, i18n]);
 
   const version = useMemo(() => {
     const releasesDates = Object.keys(releases);
