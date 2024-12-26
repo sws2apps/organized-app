531e9037fcc48b21da991011e7516d4f3ca2977e
rhahao <26148770+rhahao@users.noreply.github.com>
2024-12-26 05:02:16 +0300

    chore(demo): allow testing source language update

--- src/features/congregation/settings/meeting_forms/source_language/useSourceLanguage.tsx
+++ src/features/congregation/settings/meeting_forms/source_language/useSourceLanguage.tsx
@@ -1,31 +1,17 @@
 import { useMemo } from 'react';
-import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { useRecoilValue } from 'recoil';
 import {
   JWLangState,
   settingsState,
   userDataViewState,
 } from '@states/settings';
-import { LANGUAGE_LIST } from '@constants/index';
+import { isDemo, LANGUAGE_LIST } from '@constants/index';
 import { dbAppSettingsUpdate } from '@services/dexie/settings';
-import { isOnlineState } from '@states/app';
-import { apiFetchSources } from '@services/api/sources';
-import { sourcesImportJW } from '@services/app/sources';
-import { songsBuildList } from '@services/i18n/songs';
-import { songsState } from '@states/songs';
-import { publicTalksState } from '@states/public_talks';
-import { assignmentsHistoryState } from '@states/schedules';
-import { publicTalksBuildList } from '@services/i18n/public_talks';
-import { schedulesBuildHistoryList } from '@services/app/schedules';
 
 const useSourceLanguage = () => {
-  const setSongs = useSetRecoilState(songsState);
-  const setPublicTalks = useSetRecoilState(publicTalksState);
-  const setAssignmentsHistory = useSetRecoilState(assignmentsHistoryState);
-
   const value = useRecoilValue(JWLangState);
   const dataView = useRecoilValue(userDataViewState);
   const settings = useRecoilValue(settingsState);
-  const isOnline = useRecoilValue(isOnlineState);
 
   const SOURCE_LANGUAGES = useMemo(() => {
     return LANGUAGE_LIST.filter((record) => record.source);
@@ -65,28 +51,11 @@ const useSourceLanguage = () => {
         'cong_settings.source_material.language': updateSourceLanguage,
       });
 
-      if (isOnline) {
-        const { data, status } = await apiFetchSources();
-        if (status === 200 && data?.length) {
-          await sourcesImportJW(data);
-        }
+      if (isDemo) {
+        localStorage.setItem('demo_source_language', value);
       }
 
-      const sourceLocale = LANGUAGE_LIST.find(
-        (record) => record.code.toUpperCase() === value
-      ).locale;
-
-      // reload songs
-      const songs = await songsBuildList(sourceLocale);
-      setSongs(songs);
-
-      // reload public talks
-      const talks = await publicTalksBuildList(sourceLocale);
-      setPublicTalks(talks);
-
-      // reload assignment history
-      const history = await schedulesBuildHistoryList();
-      setAssignmentsHistory(history);
+      location.href = '/';
     } catch (error) {
       console.error(error);
     }
