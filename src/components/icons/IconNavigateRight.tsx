919b7849b1a5cfffcc474745303ca481b9819b64
Max Makaluk <70341920+FussuChalice@users.noreply.github.com>
2025-01-05 08:10:34 +0200

    feat(meetings): add navigation arrows between weeks

---
+++ src/components/icons/IconNavigateRight.tsx
@@ -0,0 +1,52 @@
+import { SvgIcon, SxProps, Theme } from '@mui/material';
+
+type IconProps = {
+  color?: string;
+  width?: number;
+  height?: number;
+  sx?: SxProps<Theme>;
+  className?: string;
+};
+
+const IconNavigateRight = ({
+  color = '#222222',
+  width = 24,
+  height = 24,
+  sx = {},
+  className,
+}: IconProps) => {
+  return (
+    <SvgIcon
+      className={`organized-icon-navigate-right ${className}`}
+      sx={{ width: `${width}px`, height: `${height}px`, ...sx }}
+    >
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <mask
+          id="mask0_13543_223944"
+          style={{ maskType: 'alpha' }}
+          maskUnits="userSpaceOnUse"
+          x="0"
+          y="0"
+          width="24"
+          height="24"
+        >
+          <rect width="24" height="24" fill="#D9D9D9" />
+        </mask>
+        <g mask="url(#mask0_13543_223944)">
+          <path
+            d="M7.41942 21.6538L17.0732 12L7.41942 2.34619L6.00022 3.76539L14.2348 12L6.00022 20.2346L7.41942 21.6538Z"
+            fill={color}
+          />
+        </g>
+      </svg>
+    </SvgIcon>
+  );
+};
+
+export default IconNavigateRight;
