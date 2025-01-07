acac7703f2aaccd9971443b9da7de36269a76061
Max Makaluk <m.makaliuk@gmail.com>
2025-01-07 13:47:31 +0200

    feat(components): combine profile + dropdown  – new approach | task CU-86c0wmhz4

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import AppRoot from './RootWrap';
+import { getCSSPropertyValue } from '@utils/common';
 
 const theme = localStorage.getItem('theme') || 'light';
 const color = localStorage.getItem('color') || 'blue';
@@ -8,9 +9,8 @@ const newTheme = `${color}-${theme}`;
 
 document.documentElement.setAttribute('data-theme', newTheme);
 
-const themeColor = getComputedStyle(document.documentElement).getPropertyValue(
-  '--accent-100'
-);
+const themeColor = getCSSPropertyValue('--accent-100');
+
 document
   .querySelector("meta[name='theme-color']")
   .setAttribute('content', themeColor);
