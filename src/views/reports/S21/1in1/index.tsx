3e6a6a2d086b1b1bfadbee1baec9250fbfe43703
rhahao.vj <26148770+rhahao@users.noreply.github.com>
2025-01-08 06:00:35 +0000

    fix(templates): font registration for S-21 cards

--- src/views/reports/S21/1in1/index.tsx
+++ src/views/reports/S21/1in1/index.tsx
@@ -1,7 +1,22 @@
-import { Document, Page } from '@react-pdf/renderer';
+import { Document, Font, Page } from '@react-pdf/renderer';
 import { S21Type } from '../shared/index.types';
 import { styles } from '../shared/index.styles';
 import CardS21 from '../shared/Card21';
+import FontLight from '@assets/fonts/Inter-Light.ttf';
+import FontRegular from '@assets/fonts/Inter-Regular.ttf';
+import FontMedium from '@assets/fonts/Inter-Medium.ttf';
+import FontSemiBold from '@assets/fonts/Inter-SemiBold.ttf';
+
+Font.register({
+  family: 'Inter',
+  format: 'truetype',
+  fonts: [
+    { src: FontLight, fontWeight: 'light' },
+    { src: FontRegular, fontWeight: 'normal' },
+    { src: FontMedium, fontWeight: 'medium' },
+    { src: FontSemiBold, fontWeight: 'semibold' },
+  ],
+});
 
 const TemplateS21Doc1in1 = ({ data }: S21Type) => {
   return (
