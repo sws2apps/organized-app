93d7c3bdac38a5a198393e580ce347de16a3edc3
rhahao <26148770+rhahao@users.noreply.github.com>
2025-01-25 23:07:03 +0300

    feat(congregation): allow import congregation and user settings

--- src/features/congregation/settings/import_export/confirm_import/index.types.ts
+++ src/features/congregation/settings/import_export/confirm_import/index.types.ts
@@ -5,6 +5,7 @@ import { FieldServiceGroupType } from '@definition/field_service_groups';
 import { MeetingAttendanceType } from '@definition/meeting_attendance';
 import { PersonType } from '@definition/person';
 import { SchedWeekType } from '@definition/schedules';
+import { SettingsType } from '@definition/settings';
 import { SourceWeekType } from '@definition/sources';
 import { SpeakersCongregationsType } from '@definition/speakers_congregations';
 import { UserBibleStudyType } from '@definition/user_bible_studies';
@@ -23,7 +24,9 @@ export type ImportFieldType =
   | 'cong_field_service_reports'
   | 'meeting_attendance'
   | 'midweek_history'
-  | 'weekend_history';
+  | 'weekend_history'
+  | 'cong_settings'
+  | 'user_settings';
 
 export type ImportChoiceType = Record<ImportFieldType, boolean>;
 
@@ -40,4 +43,6 @@ export type ImportDbType = {
   meeting_attendance?: MeetingAttendanceType[];
   sources?: SourceWeekType[];
   sched?: SchedWeekType[];
+  cong_settings?: SettingsType['cong_settings'];
+  user_settings?: SettingsType['user_settings'];
 };
