a36c42e713edc32c0d1bbf1fbd978751ce0c3dca
rhahao <26148770+rhahao@users.noreply.github.com>
2025-02-13 16:30:46 +0300

    chore(app): apply code fixes

--- src/features/ministry/service_year/yearly_stats/hours_stats/useHoursStats.tsx
+++ src/features/ministry/service_year/yearly_stats/hours_stats/useHoursStats.tsx
@@ -1,11 +1,6 @@
-import { useMemo } from 'react';
-import { useCurrentUser } from '@hooks/index';
 import useMinistryYearlyRecord from '@features/ministry/hooks/useMinistryYearlyRecord';
-import { AssignmentCode } from '@definition/assignment';
 
 const useHoursStats = (year: string) => {
-  const { person } = useCurrentUser();
-
   const { hours, isFR } = useMinistryYearlyRecord(year);
 
   return { isFR, hours };
