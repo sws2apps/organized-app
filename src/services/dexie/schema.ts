4506654238f79f464b492830e6528fbdc0ca3f29
rhahao <26148770+rhahao@users.noreply.github.com>
2025-03-25 19:35:17 +0300

    chore(dashboard): update manual sync action

--- src/services/dexie/schema.ts
+++ src/services/dexie/schema.ts
@@ -343,7 +343,7 @@ export const settingSchema: SettingsType = {
     account_type: '',
     backup_automatic: {
       enabled: { value: true, updatedAt: '' },
-      interval: { value: 15, updatedAt: '' },
+      interval: { value: 5, updatedAt: '' },
     },
     hour_credits_enabled: { value: false, updatedAt: '' },
     firstname: { value: '', updatedAt: '' },
