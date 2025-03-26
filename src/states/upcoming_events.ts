aca1c358ebcd1620ea29da44049ba3f7b359a035
Max Makaluk <70341920+FussuChalice@users.noreply.github.com>
2025-03-26 18:13:20 +0200

    fix(states): remove unnecesary code for upcomingEventsInScope state

--- src/states/upcoming_events.ts
+++ src/states/upcoming_events.ts
@@ -2,18 +2,9 @@
 This file holds the source of the truth from the table "upcoming_events".
 */
 import { UpcomingEventType } from '@definition/upcoming_events';
-import { atom, selector } from 'recoil';
+import { atom } from 'recoil';
 
 export const upcomingEventsState = atom<UpcomingEventType[]>({
   key: 'upcomingEvents',
   default: [],
-});
-
-export const upcomingEventsInScopeState = selector({
-  key: 'upcomingEventsInScope',
-  get: ({ get }) => {
-    const upcomingEvents = get(upcomingEventsState);
-
-    return upcomingEvents.filter((event) => event.event_data.scope !== '');
-  },
 });
