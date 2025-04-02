5cd82cb1917b11e25df8e61a4afb745415f31042
Max Makaluk <70341920+FussuChalice@users.noreply.github.com>
2025-04-02 13:52:07 +0300

    fix(congregation): link updatedAt field with another upcoming event fields

--- src/features/congregation/upcoming_events/edit_upcoming_event/useEditUpcomingEvent.tsx
+++ src/features/congregation/upcoming_events/edit_upcoming_event/useEditUpcomingEvent.tsx
@@ -10,6 +10,7 @@ const useEditUpcomingEvent = ({ data, onSave }: EditUpcomingEventProps) => {
       const updatedEvents = [...prev];
       updatedEvents[eventIndex] = {
         ...updatedEvents[eventIndex],
+        updatedAt: new Date().toISOString(),
         event_data: {
           ...updatedEvents[eventIndex].event_data,
           date: value.toISOString(),
@@ -24,6 +25,7 @@ const useEditUpcomingEvent = ({ data, onSave }: EditUpcomingEventProps) => {
       const updatedEvents = [...prev];
       updatedEvents[eventIndex] = {
         ...updatedEvents[eventIndex],
+        updatedAt: new Date().toISOString(),
         event_data: {
           ...updatedEvents[eventIndex].event_data,
           time: value.toISOString(),
@@ -41,6 +43,7 @@ const useEditUpcomingEvent = ({ data, onSave }: EditUpcomingEventProps) => {
       const updatedEvents = [...prev];
       updatedEvents[eventIndex] = {
         ...updatedEvents[eventIndex],
+        updatedAt: new Date().toISOString(),
         event_data: {
           ...updatedEvents[eventIndex].event_data,
           additional: value,
@@ -55,6 +58,7 @@ const useEditUpcomingEvent = ({ data, onSave }: EditUpcomingEventProps) => {
       const updatedEvents = [...prev];
       updatedEvents[eventIndex] = {
         ...updatedEvents[eventIndex],
+        updatedAt: new Date().toISOString(),
         event_data: {
           ...updatedEvents[eventIndex].event_data,
           custom: value,
@@ -72,6 +76,7 @@ const useEditUpcomingEvent = ({ data, onSave }: EditUpcomingEventProps) => {
       const updatedEvents = [...prev];
       updatedEvents[eventIndex] = {
         ...updatedEvents[eventIndex],
+        updatedAt: new Date().toISOString(),
         event_data: {
           ...updatedEvents[eventIndex].event_data,
           type: value,
@@ -86,6 +91,7 @@ const useEditUpcomingEvent = ({ data, onSave }: EditUpcomingEventProps) => {
       const updatedEvents = [...prev];
       updatedEvents[eventIndex] = {
         ...updatedEvents[eventIndex],
+        updatedAt: new Date().toISOString(),
         _deleted: true,
       };
       return updatedEvents;
