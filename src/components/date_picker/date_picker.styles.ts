1ea0d21770e309fe3c7d5708765acb3037215a9d
Max Makaluk <70341920+FussuChalice@users.noreply.github.com>
2025-04-08 21:07:10 +0300

    fix(components): apply custom color to inactive dates in datepicker

--- src/components/date_picker/date_picker.styles.ts
+++ src/components/date_picker/date_picker.styles.ts
@@ -79,6 +79,11 @@ export const StyleDatePickerPopper = {
   '.Mui-selected': {
     backgroundColor: 'var(--accent-main) !important',
   },
+
+  '.Mui-disabled': {
+    color: 'var(--grey-200) !important',
+  },
+
   '.MuiSvgIcon-root': {
     color: 'var(--grey-400)',
   },
