ae4392d0c9bb97ddf7ea982d4db9599c7011712b
rhahao <26148770+rhahao@users.noreply.github.com>
2025-05-21 21:38:00 +0300

    perf(persons): simplify person categories saving

--- src/features/persons/button_actions/useButtonActions.tsx
+++ src/features/persons/button_actions/useButtonActions.tsx
@@ -191,13 +191,14 @@ const useButtonActions = () => {
       }
 
       if (dataView !== 'main') {
-        const viewExist =
-          person.person_data.categories.value.includes(dataView);
+        const categories = Array.from(
+          new Set([...person.person_data.categories.value, dataView])
+        );
 
-        if (!viewExist) {
-          person.person_data.categories.value.push(dataView);
-          person.person_data.categories.updatedAt = new Date().toISOString();
-        }
+        person.person_data.categories = {
+          value: categories,
+          updatedAt: new Date().toISOString(),
+        };
       }
 
       await dbPersonsSave(person, isNewPerson);
