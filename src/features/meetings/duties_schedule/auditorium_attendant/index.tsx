4a71672a72b297e3690ea7b5a0f6ea55bad6e8fd
rhahao <26148770+rhahao@users.noreply.github.com>
2025-06-09 00:29:37 +0300

    add person selector for fixed assignment

--- src/features/meetings/duties_schedule/auditorium_attendant/index.tsx
+++ src/features/meetings/duties_schedule/auditorium_attendant/index.tsx
@@ -1,16 +1,41 @@
-import { Stack } from '@mui/material';
+import { Grid, Stack } from '@mui/material';
 import { IconHallOverseer } from '@components/icons';
-import { useAppTranslation } from '@hooks/index';
+import { useAppTranslation, useBreakpoints } from '@hooks/index';
+import useAuditoriumAttendant from './useAuditoriumAttendant';
 import DutyName from '../duty_name';
+import PersonSelector from '@features/meetings/person_selector';
+import { AssignmentCode } from '@definition/assignment';
 
 const AuditoriumAttendant = () => {
   const { t } = useAppTranslation();
+
+  const { laptopDown } = useBreakpoints();
+
+  const { week } = useAuditoriumAttendant();
+
   return (
-    <Stack spacing="8px">
+    <Stack
+      spacing={laptopDown ? '24px' : '8px'}
+      direction={laptopDown ? 'column' : 'row'}
+      alignItems="flex-start"
+    >
       <DutyName
         duty={t('tr_dutiesAuditoriumAttendant')}
         icon={<IconHallOverseer color="var(--accent-dark)" />}
       />
+
+      <Stack spacing="8px" flex={1} width="100%">
+        <Grid container spacing={2}>
+          <Grid size={{ mobile: 12, laptop: 6 }}>
+            <PersonSelector
+              label={t('tr_brother')}
+              week={week}
+              assignment="DUTIES_AuditoriumAttendant"
+              type={AssignmentCode.DUTIES_AuditoriumAttendant}
+            />
+          </Grid>
+        </Grid>
+      </Stack>
     </Stack>
   );
 };
--- src/features/meetings/duties_schedule/stage/index.tsx
+++ src/features/meetings/duties_schedule/stage/index.tsx
@@ -1,16 +1,41 @@
-import { Stack } from '@mui/material';
+import { Grid, Stack } from '@mui/material';
 import { IconTalk } from '@components/icons';
-import { useAppTranslation } from '@hooks/index';
+import { AssignmentCode } from '@definition/assignment';
+import { useAppTranslation, useBreakpoints } from '@hooks/index';
+import useStage from './useStage';
 import DutyName from '../duty_name';
+import PersonSelector from '@features/meetings/person_selector';
 
 const Stage = () => {
   const { t } = useAppTranslation();
+
+  const { laptopDown } = useBreakpoints();
+
+  const { week } = useStage();
+
   return (
-    <Stack spacing="8px">
+    <Stack
+      spacing={laptopDown ? '24px' : '8px'}
+      direction={laptopDown ? 'column' : 'row'}
+      alignItems="flex-start"
+    >
       <DutyName
         duty={t('tr_dutiesStage')}
         icon={<IconTalk color="var(--accent-dark)" />}
       />
+
+      <Stack spacing="8px" flex={1} width="100%">
+        <Grid container spacing={2}>
+          <Grid size={{ mobile: 12, laptop: 6 }}>
+            <PersonSelector
+              label={t('tr_brother')}
+              week={week}
+              assignment="DUTIES_Stage"
+              type={AssignmentCode.DUTIES_Stage}
+            />
+          </Grid>
+        </Grid>
+      </Stack>
     </Stack>
   );
 };
