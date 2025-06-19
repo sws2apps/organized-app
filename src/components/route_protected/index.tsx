9cb3555003f9ababe265e0aca7ab74774c7872e6
Angelo Annunziata <ing.annunziata@gmail.com>
2025-06-19 11:49:20 +0200

    feat(field_service_meetings): adds feature UI

--- src/components/route_protected/index.tsx
+++ src/components/route_protected/index.tsx
@@ -13,13 +13,8 @@ const RouteProtected = ({
   const FEATURE_FLAGS = useAtomValue(featureFlagsState);
 
   const render = useMemo(() => {
-    if (!flag) return allowed;
-
-    const flagValue = FEATURE_FLAGS[flag];
-
-    if (allowed === undefined) return flagValue;
-
-    return allowed ? flagValue : false;
+    const flagValue = flag ? FEATURE_FLAGS[flag] : allowed;
+    return allowed === undefined ? flagValue : allowed && flagValue;
   }, [allowed, flag, FEATURE_FLAGS]);
 
   return render ? <Outlet /> : <Navigate to="/" />;
