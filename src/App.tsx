c3dbecb870ec507862655999ff0d2b957a41e0be
rhahao <26148770+rhahao@users.noreply.github.com>
2025-10-27 21:04:35 +0300

    fix(dashboard): public talk coordinator cannot open speakers catalog

--- src/App.tsx
+++ src/App.tsx
@@ -156,13 +156,24 @@ const App = ({ updatePwa }: { updatePwa: VoidFunction }) => {
               ],
             },
 
+            // public talk coordinator roues
+            {
+              element: (
+                <RouteProtected
+                  allowed={isAppointed || isPublicTalkCoordinator}
+                />
+              ),
+              children: [
+                { path: '/speakers-catalog', element: <SpeakersCatalog /> },
+              ],
+            },
+
             // elder routes
             {
               element: <RouteProtected allowed={isElder} />,
               children: [
                 { path: '/persons', element: <PersonsAll /> },
                 { path: '/persons/:id', element: <PersonDetails /> },
-                { path: '/speakers-catalog', element: <SpeakersCatalog /> },
                 {
                   path: '/congregation-settings',
                   element: <CongregationSettings />,
