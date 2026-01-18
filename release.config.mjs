666407d91836b82ec68fee4b87cdf8ec31ead926
rhahao <26148770+rhahao@users.noreply.github.com>
2026-01-18 15:51:02 +0300

    build(app): prevent semantic-release from updating changelog

--- release.config.mjs
+++ release.config.mjs
@@ -40,7 +40,6 @@ export default {
     [
       '@semantic-release/git',
       {
-        assets: ['package.json', 'CHANGELOG.md'],
         message:
           'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
