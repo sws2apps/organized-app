9f13efdbfe01cfed1b4240d821d29060c98c6bcd
rhahao <26148770+rhahao@users.noreply.github.com>
2026-01-18 16:09:47 +0300

    config(semantic-release): define empty array for git assets

--- release.config.mjs
+++ release.config.mjs
@@ -40,6 +40,7 @@ export default {
     [
       '@semantic-release/git',
       {
+        assets: [],
         message:
           'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
