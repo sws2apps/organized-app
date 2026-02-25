6413585e3dbd80e7c1e2ef75d2e0513fbc57b7fa
rhahao <26148770+rhahao@users.noreply.github.com>
2026-02-25 21:12:58 +0300

    fix(persons): service unable to approve auxiliary application

--- src/features/ministry/application_form/sc_member/useSCMember.tsx
+++ src/features/ministry/application_form/sc_member/useSCMember.tsx
@@ -26,7 +26,7 @@ const useSCMember = ({ type, application }: CommitteeMemberProps) => {
       return t('tr_secretary');
     }
 
-    if (type === 'service') {
+    if (type === 'service_overseer') {
       return t('tr_serviceOverseer');
     }
   }, [type, t]);
