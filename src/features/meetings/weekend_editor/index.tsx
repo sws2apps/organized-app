07813064c726139abe5520cf39bb1f8dd4bea6fb
ux-git <ux-git@users.noreply.github.com>
2026-04-06 17:55:56 +0200

    Fix Special Talk visiting speaker dropdown

--- src/features/meetings/weekend_editor/index.tsx
+++ src/features/meetings/weekend_editor/index.tsx
@@ -16,6 +16,7 @@ import {
   WEEKEND_WITH_SPECIAL_TALK,
   WEEKEND_WITH_STANDARD_TALK,
   WEEKEND_WITH_TALKS,
+  WEEKEND_WITH_TALKS_NOCO,
   WEEKEND_WITH_WTSTUDY,
 } from '@constants/index';
 import { Week } from '@definition/week_type';
@@ -261,14 +262,13 @@ const WeekendEditor = () => {
                         }
                         jwStreamRecording={talkType === 'jwStreamRecording'}
                         visitingSpeaker={
-                          weekType === Week.NORMAL &&
+                          WEEKEND_WITH_TALKS_NOCO.includes(weekType) &&
                           talkType === 'visitingSpeaker'
                         }
                         circuitOverseer={weekType === Week.CO_VISIT}
                         talk={selectedTalk?.talk_number}
                         helperNode={
-                          (weekType === Week.NORMAL ||
-                            weekType === Week.PUBLIC_TALK) &&
+                          WEEKEND_WITH_TALKS_NOCO.includes(weekType) &&
                           talkType === 'visitingSpeaker' && (
                             <Markup
                               content={t('tr_visitinSpeakerHelpText')}
